3990b13924f9c3585471c13828aeeee0578fddd3
Le Anh Tai <leanhtai01@gmail.com>
2020-04-09 22:27:07 +0700

    improve way to select mirror

--- install_script.sh
+++ install_script.sh
@@ -94,7 +94,7 @@ mkdir /mnt/boot
 mount /dev/${install_dev}${part}1 /mnt/boot
 
 # select the mirrors
-linum=$(sed -n '/f.archlinuxvn.org/=' /etc/pacman.d/mirrorlist) # find a line and get line number
+linum=$(sed -n '/^Server = http:\/\/f.archlinuxvn.org\/archlinux\/\$repo\/os\/\$arch$/=' /etc/pacman.d/mirrorlist) # find a line and get line number
 preferredmirror=$(sed -n "$linum"p /etc/pacman.d/mirrorlist) # get line know line number
 sed -i '6 a ## My preferred mirrors' /etc/pacman.d/mirrorlist # insert line after
 sed -i "7 a $preferredmirror" /etc/pacman.d/mirrorlist
