42005d047a02ab0b23d9ce1b7f60e68214254ec3
Le Anh Tai <leanhtai01@gmail.com>
2020-04-11 15:55:23 +0700

    remove blank line in file install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -160,4 +160,3 @@ arch-chroot /mnt sed -i "${linum}s/filesystems/& resume/" /etc/mkinitcpio.conf
 arch-chroot /mnt mkinitcpio -p linux
 swapuuidvalue=$(arch-chroot /mnt blkid -s UUID -o value /dev/${install_dev}${part}2)
 echo "options resume=UUID=${swapuuidvalue}" >> /mnt/boot/loader/entries/archlinux.conf
-
