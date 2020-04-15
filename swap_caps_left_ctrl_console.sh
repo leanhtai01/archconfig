2eb690a4b5bb9214de2b9cc3952b35490b476fb1
Le Anh Tai <leanhtai01@gmail.com>
2020-04-15 23:11:57 +0700

    fix bug caused by double quotes

--- swap_caps_left_ctrl_console.sh
+++ swap_caps_left_ctrl_console.sh
@@ -10,13 +10,13 @@ mv /usr/local/share/kbd/keymaps/us.map /usr/local/share/kbd/keymaps/personal.map
 
 # make the Right Alt key same as Left Alt key 
 linum=$(sed -n '/^include "linux-with-alt-and-altgr"$/=' /usr/local/share/kbd/keymaps/personal.map)
-sed -i '${linum} a include "linux-with-two-alt-keys"' /usr/local/share/kbd/keymaps/personal.map
+sed -i "${linum} a include \"linux-with-two-alt-keys\"" /usr/local/share/kbd/keymaps/personal.map
 
 # swap Caps_Lock and Left Control
 linum=$(sed -n '/^keycode  29 = Control$/=' /usr/local/share/kbd/keymaps/personal.map)
-sed -i '${linum}s/Control/Caps_Lock/' /usr/local/share/kbd/keymaps/personal.map
+sed -i "${linum}s/Control/Caps_Lock/" /usr/local/share/kbd/keymaps/personal.map
 linum=$(sed -n '/^keycode  58 = Caps_Lock$/=' /usr/local/share/kbd/keymaps/personal.map)
-sed -i '${linum}s/Caps_Lock/Control/' /usr/local/share/kbd/keymaps/personal.map
+sed -i "${linum}s/Caps_Lock/Control/" /usr/local/share/kbd/keymaps/personal.map
 
 # make use of the personal keymap
 loadkeys /usr/local/share/kbd/keymaps/personal.map
