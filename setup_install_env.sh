9ef551ab097e1b8d335bc0912d489fd23c066253
Le Anh Tai <leanhtai01@gmail.com>
2020-04-27 10:30:23 +0700

    re-organize file

--- setup_install_env.sh
+++ setup_install_env.sh
@@ -5,10 +5,12 @@ set -e
 # swap caplocks - left ctrl
 wget https://raw.githubusercontent.com/leanhtai01/archlinuxconfiguration/master/swap_caps_left_ctrl_console.sh
 bash swap_caps_left_ctrl_console.sh
+rm swap_caps_left_ctrl_console.sh
 
 # setup mirrors
 wget https://raw.githubusercontent.com/leanhtai01/archlinuxconfiguration/master/setup_mirrors.sh
 bash setup_mirrors.sh
+rm setup_mirrors.sh
 
 # install git
 pacman -Sy --noconfirm git
@@ -19,6 +21,3 @@ pacman -Sy --noconfirm git
 
 # clone archlinuxconfigurion repo
 git clone https://github.com/leanhtai01/archlinuxconfiguration
-
-# remove unused file
-rm swap_caps_left_ctrl_console.sh setup_mirrors.sh
