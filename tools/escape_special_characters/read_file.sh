eeb7948a86cdbaea13f66e75cd122d68f8a6fc18
Le Anh Tai <leanhtai01@gmail.com>
2020-04-28 21:13:06 +0700

    output only result string

--- tools/escape_special_characters/read_file.sh
+++ tools/escape_special_characters/read_file.sh
@@ -5,6 +5,5 @@ mycommand=
 while read line
 do
     str=$(./add_backslash.sh "$line")
-    mycommand="sed -i \"/${str}/s/\/\/ //\" /usr/share/webapps/phpMyAdmin/config.inc.php"
-    echo $mycommand
+    echo $str
 done < $1
