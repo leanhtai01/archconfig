0871a615a79cddce709f9748336878f8aad41e9f
Le Anh Tai <leanhtai01@gmail.com>
2020-04-28 13:31:15 +0700

    add, remove some packages

--- install_packages.sh
+++ install_packages.sh
@@ -5,6 +5,7 @@ set -e
 # install and configure some packages, services
 # system packages
 arch-chroot /mnt pacman -Syu --needed --noconfirm lm_sensors ufw gparted dosfstools ntfs-3g p7zip unrar smartmontools screen psensor
+arch-chroot /mnt systemctl enable ufw
 
 # fonts
 arch-chroot /mnt pacman -Syu --needed --noconfirm ttf-dejavu ttf-liberation adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-serif-cn-fonts adobe-source-han-serif-tw-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts noto-fonts-emoji
@@ -14,33 +15,20 @@ arch-chroot /mnt pacman -Syu --needed --noconfirm modemmanager usb_modeswitch wv
 arch-chroot /mnt systemctl enable ModemManager
 
 # audio
-arch-chroot /mnt pacman -Syu --needed --noconfirm pulseaudio-alsa alsa-utils
+arch-chroot /mnt pacman -Syu --needed --noconfirm pulseaudio-alsa alsa-utils alsa-plugins pulseaudio-bluetooth
 
 # desktop environment
 arch-chroot /mnt pacman -Syu --needed --noconfirm xorg-server xorg-xrandr lib32-vulkan-icd-loader vulkan-icd-loader vulkan-intel xf86-video-intel lib32-mesa mesa mesa-demos
 
-# # GNOME
-# arch-chroot /mnt pacman -Syu --needed --noconfirm gnome alacarte file-roller seahorse gst-libav seahorse-nautilus gnome-clocks gnome-calendar rhythmbox gprename gnuchess transmission-gtk chrome-gnome-shell gedit gedit-plugins gnome-sound-recorder gnome-tweaks gnome-builder devhelp dconf-editor
-# arch-chroot /mnt pacman -Syu --needed --noconfirm gnome-extra
-# arch-chroot /mnt systemctl enable gdm
-
 # Plasma
-arch-chroot /mnt pacman -Syu --needed --noconfirm plasma digikam ktorrent ark dolphin dolphin-plugins ffmpegthumbs gwenview kalarm kamoso kate kcalc kdegraphics-mobipocket kdegraphics-thumbnailers kdf khelpcenter kleopatra kmousetool knotes kolourpaint kompare konsole krdc krfb kruler ksystemlog ktimer kwalletmanager okular spectacle sweeper umbrello gnome-disk-utility
-# arch-chroot /mnt pacman -Syu --needed --noconfirm kde-applications
+arch-chroot /mnt pacman -Syu --needed --noconfirm plasma digikam ktorrent ark dolphin dolphin-plugins ffmpegthumbs gwenview kalarm kamoso kate kcalc kdegraphics-mobipocket kdegraphics-thumbnailers kdf khelpcenter kleopatra kmousetool knotes kolourpaint kompare konsole krdc krfb kruler ksystemlog kwalletmanager okular spectacle sweeper umbrello gnome-disk-utility gnome-clocks
 arch-chroot /mnt systemctl enable sddm
 
 # browsers
-# arch-chroot /mnt pacman -Syu --needed --noconfirm chromium
-# arch-chroot /mnt pacman -Syu --needed --noconfirm opera
 arch-chroot /mnt pacman -Syu --needed --noconfirm firefox-developer-edition
 
 # editors
-# arch-chroot /mnt pacman -Syu --needed --noconfirm atom
-# arch-chroot /mnt pacman -Syu --needed --noconfirm geany
-arch-chroot /mnt pacman -Syu --needed --noconfirm nano
-arch-chroot /mnt pacman -Syu --needed --noconfirm vi
-arch-chroot /mnt pacman -Syu --needed --noconfirm emacs
-arch-chroot /mnt pacman -Syu --needed --noconfirm gvim
+arch-chroot /mnt pacman -Syu --needed --noconfirm nano vi emacs gvim
 
 # programming packages
 arch-chroot /mnt pacman -Syu --needed --noconfirm gdb cmake jdk-openjdk jdk8-openjdk r swi-prolog qtcreator qt5-doc qt5-examples opencv opencv-samples git go dotnet-sdk lazarus-qt5 intellij-idea-community-edition pycharm-community-edition valgrind eclipse-java tk dia
@@ -49,18 +37,25 @@ arch-chroot /mnt pacman -Syu --needed --noconfirm gdb cmake jdk-openjdk jdk8-ope
 arch-chroot /mnt pacman -Syu --needed --noconfirm wget youtube-dl reflector wimlib expect
 
 # office and learning
-# arch-chroot /mnt pacman -Syu --needed --noconfirm librecad qcad lilypond gnucash octave
-arch-chroot /mnt pacman -Syu --needed --noconfirm libreoffice-fresh gimp inkscape calibre klavaro kchmviewer geogebra freemind irssi thunderbird
+arch-chroot /mnt pacman -Syu --needed --noconfirm librecad qcad lilypond gnucash octave libreoffice-fresh gimp inkscape calibre klavaro kchmviewer geogebra freemind irssi thunderbird
 
 # multimedia
-arch-chroot /mnt pacman -Syu --needed --noconfirm obs-studio vlc
-# arch-chroot /mnt pacman -Syu --needed --noconfirm kdenlive frei0r-plugins audacity aegisub kid3 kodi
+arch-chroot /mnt pacman -Syu --needed --noconfirm obs-studio vlc kdenlive frei0r-plugins audacity aegisub kid3 kodi
 
 # virtualbox
 arch-chroot /mnt pacman -Syu --needed --noconfirm virtualbox virtualbox-guest-iso virtualbox-host-dkms
 
-# games
-# arch-chroot /mnt pacman -Syu --needed --noconfirm supertuxkart wesnoth minetest minetest-server teeworlds kbounce kigo bovo quadrapassel steam steam-native-runtime
+# wireshark
+arch-chroot /mnt pacman -Syu --needed --noconfirm wireshark-qt
+arch-chroot /mnt gpasswd -a leanhtai01 wireshark
 
-# Wine
-arch-chroot /mnt pacman -Syu --needed --noconfirm wine wine-gecko wine-mono
+# docker
+arch-chroot /mnt pacman -Syu --needed --noconfirm docker
+arch-chroot /mnt systemctl enable docker
+arch-chroot /mnt gpasswd -a leanhtai01 docker
+
+# games
+arch-chroot /mnt pacman -Syu --needed --noconfirm supertuxkart wesnoth minetest minetest-server teeworlds kbounce kigo bovo quadrapassel steam steam-native-runtime
+
+# wine
+arch-chroot /mnt pacman -Syu --needed --noconfirm wine wine-gecko wine-mono lib32-alsa-lib lib32-alsa-plugins lib32-libpulse lib32-openal lib32-mpg123 lib32-giflib lib32-libpng lib32-gnutls lib32-gst-plugins-base lib32-gst-plugins-good lib32-libldap
