484a74323671eb50478ed2149fd804b2d44508d2
Le Anh Tai <leanhtai01@gmail.com>
2020-04-28 22:01:23 +0700

    improve way to setup preferred mirrors

--- setup_mirrors.sh
+++ setup_mirrors.sh
@@ -2,14 +2,4 @@
 
 set -e
 
-# current country
-linum=$(sed -n '/^Server = http:\/\/f.archlinuxvn.org\/archlinux\/\$repo\/os\/\$arch$/=' /etc/pacman.d/mirrorlist) # find a line and get line number
-preferredmirror=$(sed -n "$linum"p /etc/pacman.d/mirrorlist) # get line know line number
-sed -i '6 a ## My preferred mirrors' /etc/pacman.d/mirrorlist # insert line after
-sed -i "7 a $preferredmirror" /etc/pacman.d/mirrorlist
-
-# best of tier 1
-sed -i "8 a Server = http:\/\/ftp.sh.cvut.cz\/arch\/\$repo\/os\/\$arch$" /etc/pacman.d/mirrorlist
-
-# best of tier 2
-sed -i "9 a Server = http:\/\/mirror.i3d.net\/pub\/archlinux\/\$repo\/os\/\$arch$" /etc/pacman.d/mirrorlist
+sed -i "6 r data/preferredmirrors" /etc/pacman.d/mirrorlist
