090174511ae9351270246cacb0173d65492dbe2e
Le Anh Tai <leanhtai01@gmail.com>
2020-05-01 15:44:44 +0700

    fix setup_mirrors.sh path problem

--- system_install/setup_mirrors.sh
+++ system_install/setup_mirrors.sh
@@ -2,4 +2,6 @@
 
 set -e
 
-sed -i "6 r ../data/preferredmirrors" /etc/pacman.d/mirrorlist
+parent_dir=$(cd $(dirname $0)/..; pwd)
+
+sed -i "6 r ${parent_dir}/data/preferredmirrors" /etc/pacman.d/mirrorlist
