73fca07ae27b33079610974757178924ae2b1e1f
Le Anh Tai <leanhtai01@gmail.com>
2020-05-01 22:44:23 +0700

    add file prepare_disk_dual-boot_with_windows_10_luks_on_lvm.sh

---
+++ system_install/prepare_disk_dual-boot_with_windows_10_luks_on_lvm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+# partition the disk
+sgdisk -n 0:0:0 -t 0:8e00 -c 0:"lvm" /dev/$install_dev
+
+# create physical volume
+dd if=/dev/zero of=/dev/${install_dev}${part}5 bs=1M count=1
+pvcreate /dev/${install_dev}${part}5
+
+# create volume group
+vgcreate sys_vol_group /dev/${install_dev}${part}5
+
+# create logical volumes
+lvcreate -L `expr 2 \* $size_of_ram`G sys_vol_group -n cryptswap
+lvcreate -l +100%FREE sys_vol_group -n cryptroot
+
+# encrypt and format root partition
+printf "$storagepass1" | cryptsetup luksFormat --type luks2 /dev/sys_vol_group/cryptroot -
+printf "$storagepass1" | cryptsetup open /dev/sys_vol_group/cryptroot root -
+
+# format the partition
+mkfs.ext4 /dev/mapper/root
+
+# mount the filesystems
+mount /dev/mapper/root /mnt
+mkdir /mnt/boot
+mount /dev/${install_dev}${part}2 /mnt/boot
