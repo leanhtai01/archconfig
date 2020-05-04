0886476cd69bdf5871ebd1c93545f385b5337243
leanhtai01 <leanhtai01@gmail.com>
2020-05-04 10:33:50 +0700

    small improve make partitions for Windows 10

--- system_install/make_partitions_for_windows_10.sh
+++ system_install/make_partitions_for_windows_10.sh
@@ -4,6 +4,7 @@ set -e
 
 # global variables
 install_dev=
+part=
 
 # let user choose device to make partitions
 printf "MAKE PARTITIONS FOR WINDOWS 10\n"
@@ -14,8 +15,16 @@ then
     read install_dev
 fi
 
+if [ $install_dev = nvme0n1 ]
+then
+    part=p
+fi
+
 # partition the disk
 sgdisk -Z /dev/$install_dev
 sgdisk -n 0:0:+1G -t 0:2700 -c 0:"win_re" /dev/$install_dev
 sgdisk -n 0:0:+1G -t 0:ef00 -c 0:"efi" /dev/$install_dev
 sgdisk -n 0:0:+1G -t 0:0c01 -c 0:"ms_reserved" /dev/$install_dev
+dd if=/dev/zero of=/dev/${install_dev}${part}1 bs=1M count=1
+dd if=/dev/zero of=/dev/${install_dev}${part}2 bs=1M count=1
+dd if=/dev/zero of=/dev/${install_dev}${part}3 bs=1M count=1
