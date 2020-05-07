41f54124a504d0825b13ca60fba9e4f091b7b57c
Lê Anh Tài <leanhtai01@gmail.com>
2020-05-07 12:02:33 +0700

    add key to VMware Workstation

--- post_install/normal_user_privilege/install_vmware_workstation.sh
+++ post_install/normal_user_privilege/install_vmware_workstation.sh
@@ -7,3 +7,4 @@ sudo systemctl enable vmware-networks
 sudo systemctl enable vmware-usbarbitrator
 sudo systemctl enable vmware-hostd
 sudo modprobe -a vmw_vmci vmmon
+sudo /usr/lib/vmware/bin/vmware-vmx-debug --new-sn FF788-A1X86-08E9Q-5YN79-XV0YD
