4b6fda48bc2629d4c0bb1bb4349263477b7dc345
Lê Anh Tài <leanhtai01@gmail.com>
2020-05-10 15:43:08 +0700

    fix configure grub bug

--- system_install/configure_grub.sh
+++ system_install/configure_grub.sh
@@ -6,6 +6,6 @@ arch-chroot /mnt pacman -Syu --needed --noconfirm grub
 case $user_choice in
     1) # normal install
 	arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=archlinux
-	grub-mkconfig -o /boot/grub/grub.cfg
+	arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 	;;
 esac
