bc0e28c08b4595fb9b39c799fe4c9a184a78e761
Lê Anh Tài <leanhtai01@gmail.com>
2020-05-11 16:13:56 +0700

    small fix configure GRUB

--- system_install/configure_grub.sh
+++ system_install/configure_grub.sh
@@ -13,5 +13,5 @@ case $user_choice in
 	;;
 esac
 
-arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=archlinux
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=archlinux --recheck
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
