9eb35f7e27fdf3b906c9298e29ed9a732f41a549
Lê Anh Tài <leanhtai01@gmail.com>
2020-05-11 21:38:46 +0700

    configure GRUB dual-boot Windows 10 LVM on LUKS

--- system_install/configure_grub.sh
+++ system_install/configure_grub.sh
@@ -4,18 +4,36 @@ set -e
 
 arch-chroot /mnt pacman -Syu --needed --noconfirm grub
 
+# create keyfile for encrypted boot partition (LVM on LUKS)
+re="[25]"
+if [[ "$user_choice" =~ $re ]]
+then
+    mkdir -m 700 /mnt/etc/luks-keys
+    dd if=/dev/random of=/mnt/etc/luks-keys/cryptboot.key bs=1 count=256 status=progress
+fi
+
+cryptlvmuuidvalue=
 case $user_choice in
     2) # lvm on luks
 	cryptlvmuuidvalue=$(arch-chroot /mnt blkid -s UUID -o value /dev/${install_dev}${part}3)
-        sed -i "/^GRUB_CMDLINE_LINUX=\"\"/s/\"\"/\"cryptdevice=UUID=${cryptlvmuuidvalue}:cryptlvm root=\/dev\/sys_vol_group\/root rw\"/" /mnt/etc/default/grub
-	sed -i "/^#GRUB_ENABLE_CRYPTODISK=y/s/^#//" /mnt/etc/default/grub
-	sed -i "/^GRUB_CMDLINE_LINUX=/s/rw/resume=UUID=${swapuuidvalue} &/" /mnt/etc/default/grub
-	mkdir -m 700 /mnt/etc/luks-keys
-	dd if=/dev/random of=/mnt/etc/luks-keys/cryptboot.key bs=1 count=256 status=progress
         printf "$bootpass1" | cryptsetup luksAddKey /dev/${install_dev}${part}2 /mnt/etc/luks-keys/cryptboot.key -
 	printf "cryptboot    /dev/${install_dev}${part}2    /etc/luks-keys/cryptboot.key\n" >> /mnt/etc/crypttab
 	;;
+    5) # dual-boot with Windows 10 (LVM on LUKS)
+	cryptlvmuuidvalue=$(arch-chroot /mnt blkid -s UUID -o value /dev/${install_dev}${part}6)
+        printf "$bootpass1" | cryptsetup luksAddKey /dev/${install_dev}${part}5 /mnt/etc/luks-keys/cryptboot.key -
+	printf "cryptboot    /dev/${install_dev}${part}5    /etc/luks-keys/cryptboot.key\n" >> /mnt/etc/crypttab
+	;;
 esac
+
+# configure GRUB for LVM on LUKS
+re="[25]"
+if [[ "$user_choice" =~ $re ]]
+then
+    sed -i "/^GRUB_CMDLINE_LINUX=\"\"/s/\"\"/\"cryptdevice=UUID=${cryptlvmuuidvalue}:cryptlvm root=\/dev\/sys_vol_group\/root rw\"/" /mnt/etc/default/grub
+    sed -i "/^#GRUB_ENABLE_CRYPTODISK=y/s/^#//" /mnt/etc/default/grub
+    sed -i "/^GRUB_CMDLINE_LINUX=/s/rw/resume=UUID=${swapuuidvalue} &/" /mnt/etc/default/grub
+fi
 
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=archlinux --recheck
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
--- system_install/prepare_disk_dual-boot_with_windows_10_lvm_on_luks.sh
+++ system_install/prepare_disk_dual-boot_with_windows_10_lvm_on_luks.sh
@@ -3,14 +3,34 @@
 set -e
 
 # partition the disk
+if [ $bootloader = 2 ]
+then
+    sgdisk -n 0:0:+1G -t 0:8309 -c 0:"cryptboot" /dev/$install_dev
+fi
 sgdisk -n 0:0:0 -t 0:8309 -c 0:"cryptlvm" /dev/$install_dev
 
-# create the LUKS encrypted container
-dd if=/dev/zero of=/dev/${install_dev}${part}5 bs=1M count=1
-printf "$storagepass1" | cryptsetup luksFormat --type luks2 /dev/${install_dev}${part}5 -
+case $bootloader in
+    1) # systemd-boot
+	# create the LUKS encrypted container
+	dd if=/dev/zero of=/dev/${install_dev}${part}5 bs=1M count=1
+	printf "$storagepass1" | cryptsetup luksFormat --type luks2 /dev/${install_dev}${part}5 -
 
-# open the container
-printf "$storagepass1" | cryptsetup open /dev/${install_dev}${part}5 cryptlvm -
+	# open the container
+	printf "$storagepass1" | cryptsetup open /dev/${install_dev}${part}5 cryptlvm -
+	;;
+    2) # GRUB
+	dd if=/dev/zero of=/dev/${install_dev}${part}5 bs=1M count=1
+	printf "$bootpass1" | cryptsetup luksFormat --type luks1 /dev/${install_dev}${part}5 -
+	printf "$bootpass1" | cryptsetup open /dev/${install_dev}${part}5 cryptboot -
+	
+	# create the LUKS encrypted container
+	dd if=/dev/zero of=/dev/${install_dev}${part}6 bs=1M count=1
+	printf "$storagepass1" | cryptsetup luksFormat --type luks2 /dev/${install_dev}${part}6 -
+
+	# open the container
+	printf "$storagepass1" | cryptsetup open /dev/${install_dev}${part}6 cryptlvm -
+	;;
+esac
 
 # create a physical volume on top of the opened LUKS container
 pvcreate /dev/mapper/cryptlvm
@@ -23,11 +43,25 @@ lvcreate -L `expr 2 \* $size_of_ram`G sys_vol_group -n swap
 lvcreate -l +100%FREE sys_vol_group -n root
 
 # format the partitions
+if [ $bootloader = 2 ]
+then
+    mkfs.ext4 /dev/mapper/cryptboot
+fi
 mkswap /dev/sys_vol_group/swap
 swapon /dev/sys_vol_group/swap
 mkfs.ext4 /dev/sys_vol_group/root
 
 # mount the filesystems
 mount /dev/sys_vol_group/root /mnt
-mkdir /mnt/boot
-mount /dev/${install_dev}${part}2 /mnt/boot
+case $bootloader in
+    1) # systemd-boot
+	mkdir /mnt/boot
+	mount /dev/${install_dev}${part}2 /mnt/boot
+	;;
+    2) # GRUB
+	mkdir /mnt/boot
+	mount /dev/mapper/cryptboot /mnt/boot
+	mkdir /mnt/boot/efi
+	mount /dev/${install_dev}${part}2 /mnt/boot/efi
+	;;
+esac
