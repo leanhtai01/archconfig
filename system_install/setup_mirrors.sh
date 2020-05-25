a0e53be90555e2873c8fa25bc187531dac0319c7
leanhtai01 <leanhtai01@gmail.com>
2020-05-25 20:10:01 +0700

    separate get the best mirrors job

--- system_install/setup_mirrors.sh
+++ system_install/setup_mirrors.sh
@@ -2,10 +2,10 @@
 
 set -e
 
+current_dir=$(dirname $0)
 parent_dir=$(cd $(dirname $0)/..; pwd)
 
-mkdir pkg tmp
+mkdir pkg
 wget -O pkg/reflector.pkg.tar.zst https://www.archlinux.org/packages/community/any/reflector/download/
 pacman -U --noconfirm pkg/reflector.pkg.tar.zst
-reflector --latest 10 --protocol http --protocol https --sort rate --save tmp/mirrorlist
-sed -n '/^Server.*/,$p' tmp/mirrorlist > /etc/pacman.d/mirrorlist
+. $current_dir/get_the_best_mirrors.sh
