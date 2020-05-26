4f9aa42dfded9718e3530dc42160ff642a2ced25
leanhtai01 <leanhtai01@gmail.com>
2020-05-26 08:02:28 +0700

    get the best mirrors from closest country

--- system_install/get_the_best_mirrors.sh
+++ system_install/get_the_best_mirrors.sh
@@ -2,4 +2,16 @@
 
 set -e
 
-reflector --latest 10 --protocol http --protocol https --sort rate | sed -n '/^Server.*/,$p' > /etc/pacman.d/mirrorlist
+current_dir=$(dirname $0)
+closest_country="CN TW VN SG TH US GB RU JP IN"
+
+mkdir $current_dir/tmp
+
+for country in $closest_country
+do
+    curl -s "https://www.archlinux.org/mirrorlist/?country=$country&protocol=http&protocol=https&ip_version=4&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' >> $current_dir/tmp/closestmirrors
+done
+
+reflector --latest 5 --protocol http --protocol https --sort rate | sed -n '/^Server.*/,$p' >> $current_dir/tmp/closestmirrors
+rankmirrors -n 5 $current_dir/tmp/closestmirrors > /etc/pacman.d/mirrorlist
+
--- system_install/setup_mirrors.sh
+++ system_install/setup_mirrors.sh
@@ -6,6 +6,11 @@ current_dir=$(dirname $0)
 parent_dir=$(cd $(dirname $0)/..; pwd)
 
 mkdir pkg
+
 wget -O pkg/reflector.pkg.tar.zst https://www.archlinux.org/packages/community/any/reflector/download/
+wget -O pkg/fakeroot.pkg.tar.zst https://www.archlinux.org/packages/core/x86_64/fakeroot/download/
+wget -O pkg/pacman-contrib.pkg.tar.zst https://www.archlinux.org/packages/community/x86_64/pacman-contrib/download/
 pacman -U --noconfirm pkg/reflector.pkg.tar.zst
+pacman -U --noconfirm pkg/fakeroot.pkg.tar.zst
+pacman -U --noconfirm pkg/pacman-contrib.pkg.tar.zst
 . $current_dir/get_the_best_mirrors.sh
