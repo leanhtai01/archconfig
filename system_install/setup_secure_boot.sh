0f5f3691f1dce5fa17d7fda9ab3aea95497339cc
leanhtai01 <leanhtai01@gmail.com>
2020-05-28 14:52:31 +0700

    full setup secure boot

--- system_install/setup_secure_boot.sh
+++ system_install/setup_secure_boot.sh
@@ -2,7 +2,28 @@
 
 set -e
 
-yay -Syu --noconfirm preloader-signed
-sudo cp /usr/share/preloader-signed/{PreLoader,HashTool}.efi /boot/efi/EFI/archlinux
-sudo cp /boot/efi/EFI/archlinux/grubx64.efi /boot/efi/EFI/archlinux/loader.efi
-sudo efibootmgr --verbose --disk /dev/nvme0n1 --part 2 --create --label "archlinuxsecure" --loader /EFI/archlinux/PreLoader.efi
+parent_dir=$(cd $(dirname $0)/..; pwd)
+partnum=2
+
+re="[123]"
+if [[ "$user_choice" =~ $re ]]
+then
+    partnum=1
+fi
+
+case $bootloader in
+    1) # systemd-boot
+	cp $parent_dir/data/preloader-signed/{PreLoader,HashTool}.efi /boot/EFI/systemd
+	cp /boot/EFI/systemd/systemd-bootx64.efi /boot/EFI/systemd/loader.efi
+	efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "archlinuxsecure" --loader /EFI/systemd/PreLoader.efi
+	;;
+    2) # GRUB (encrypted boot)
+	;&
+    3) # GRUB (non-encrypted boot)
+	cp $parent_dir/data/preloader-signed/{PreLoader,HashTool}.efi /boot/efi/EFI/archlinux
+	cp /boot/efi/EFI/archlinux/grubx64.efi /boot/efi/EFI/archlinux/loader.efi
+	efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "archlinuxsecure" --loader /EFI/archlinux/PreLoader.efi
+	;;
+esac
+
+
