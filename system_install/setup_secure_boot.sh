6bc4b988d2878f4dc0c16a0be6191a60023155a7
leanhtai01 <leanhtai01@gmail.com>
2020-05-28 15:34:50 +0700

    fix path preloader-signed

--- system_install/setup_secure_boot.sh
+++ system_install/setup_secure_boot.sh
@@ -13,15 +13,15 @@ fi
 
 case $bootloader in
     1) # systemd-boot
-	arch-chroot /mnt cp $parent_dir/data/preloader-signed/{PreLoader,HashTool}.efi /boot/EFI/systemd
-	arch-chroot /mnt cp /boot/EFI/systemd/systemd-bootx64.efi /boot/EFI/systemd/loader.efi
+        cp $parent_dir/data/preloader-signed/{PreLoader,HashTool}.efi /mnt/boot/EFI/systemd
+        cp /boot/EFI/systemd/systemd-bootx64.efi /mnt/boot/EFI/systemd/loader.efi
 	arch-chroot /mnt efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "archlinuxsecure" --loader /EFI/systemd/PreLoader.efi
 	;;
     2) # GRUB (encrypted boot)
 	;&
     3) # GRUB (non-encrypted boot)
-	arch-chroot /mnt cp $parent_dir/data/preloader-signed/{PreLoader,HashTool}.efi /boot/efi/EFI/archlinux
-	arch-chroot /mnt cp /boot/efi/EFI/archlinux/grubx64.efi /boot/efi/EFI/archlinux/loader.efi
+        cp $parent_dir/data/preloader-signed/{PreLoader,HashTool}.efi /mnt/boot/efi/EFI/archlinux
+        cp /boot/efi/EFI/archlinux/grubx64.efi /mnt/boot/efi/EFI/archlinux/loader.efi
 	arch-chroot /mnt efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "archlinuxsecure" --loader /EFI/archlinux/PreLoader.efi
 	;;
 esac
