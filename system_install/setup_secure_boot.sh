9efe6fb0d5b2bd6a1b45320cffd10dceaeed8743
leanhtai01 <leanhtai01@gmail.com>
2020-05-28 15:16:49 +0700

    fix run command in arch-chroot

--- system_install/setup_secure_boot.sh
+++ system_install/setup_secure_boot.sh
@@ -13,16 +13,16 @@ fi
 
 case $bootloader in
     1) # systemd-boot
-	cp $parent_dir/data/preloader-signed/{PreLoader,HashTool}.efi /boot/EFI/systemd
-	cp /boot/EFI/systemd/systemd-bootx64.efi /boot/EFI/systemd/loader.efi
-	efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "archlinuxsecure" --loader /EFI/systemd/PreLoader.efi
+	arch-chroot /mnt cp $parent_dir/data/preloader-signed/{PreLoader,HashTool}.efi /boot/EFI/systemd
+	arch-chroot /mnt cp /boot/EFI/systemd/systemd-bootx64.efi /boot/EFI/systemd/loader.efi
+	arch-chroot /mnt efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "archlinuxsecure" --loader /EFI/systemd/PreLoader.efi
 	;;
     2) # GRUB (encrypted boot)
 	;&
     3) # GRUB (non-encrypted boot)
-	cp $parent_dir/data/preloader-signed/{PreLoader,HashTool}.efi /boot/efi/EFI/archlinux
-	cp /boot/efi/EFI/archlinux/grubx64.efi /boot/efi/EFI/archlinux/loader.efi
-	efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "archlinuxsecure" --loader /EFI/archlinux/PreLoader.efi
+	arch-chroot /mnt cp $parent_dir/data/preloader-signed/{PreLoader,HashTool}.efi /boot/efi/EFI/archlinux
+	arch-chroot /mnt cp /boot/efi/EFI/archlinux/grubx64.efi /boot/efi/EFI/archlinux/loader.efi
+	arch-chroot /mnt efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "archlinuxsecure" --loader /EFI/archlinux/PreLoader.efi
 	;;
 esac
 
