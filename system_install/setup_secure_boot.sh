ddf1ce540b5222620dc2925782e4ca3d687eb4ea
leanhtai01 <leanhtai01@gmail.com>
2020-06-01 17:09:14 +0700

    create boot entry KeyTool when setup secure boot

--- system_install/setup_secure_boot.sh
+++ system_install/setup_secure_boot.sh
@@ -11,10 +11,14 @@ then
     partnum=1
 fi
 
+arch-chroot /mnt pacman -Syu --needed --noconfirm efitools
+
 case $bootloader in
     1) # systemd-boot
         cp $parent_dir/data/preloader-signed/{PreLoader,HashTool}.efi /mnt/boot/EFI/systemd
         cp /mnt/boot/EFI/systemd/systemd-bootx64.efi /mnt/boot/EFI/systemd/loader.efi
+	arch-chroot /mnt cp /usr/share/efitools/efi/KeyTool.efi /boot/EFI/systemd/
+	arch-chroot /mnt efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "KeyTool" --loader /EFI/systemd/KeyTool.efi
 	arch-chroot /mnt efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "archlinuxsecure" --loader /EFI/systemd/PreLoader.efi
 	;;
     2) # GRUB (encrypted boot)
@@ -22,6 +26,8 @@ case $bootloader in
     3) # GRUB (non-encrypted boot)
         cp $parent_dir/data/preloader-signed/{PreLoader,HashTool}.efi /mnt/boot/efi/EFI/archlinux
         cp /mnt/boot/efi/EFI/archlinux/grubx64.efi /mnt/boot/efi/EFI/archlinux/loader.efi
+	arch-chroot /mnt cp /usr/share/efitools/efi/KeyTool.efi /boot/efi/EFI/archlinux/
+	arch-chroot /mnt efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "KeyTool" --loader /EFI/archlinux/KeyTool.efi
 	arch-chroot /mnt efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "archlinuxsecure" --loader /EFI/archlinux/PreLoader.efi
 	;;
 esac
