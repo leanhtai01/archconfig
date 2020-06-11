362811415253193bcc989c296b6fd340cc8d1d10
leanhtai01 <leanhtai01@gmail.com>
2020-06-11 08:24:04 +0700

    add package goldendict

--- system_install/install_packages.sh
+++ system_install/install_packages.sh
@@ -39,7 +39,7 @@ arch-chroot /mnt pacman -Syu --needed --noconfirm wget youtube-dl reflector wiml
 arch-chroot /mnt pacman -Syu --needed --noconfirm remmina libvncserver freerdp
 
 # office and learning
-arch-chroot /mnt pacman -Syu --needed --noconfirm librecad qcad lilypond gnucash octave libreoffice-fresh gimp inkscape calibre klavaro kchmviewer geogebra freemind irssi hexchat thunderbird
+arch-chroot /mnt pacman -Syu --needed --noconfirm librecad qcad lilypond gnucash octave libreoffice-fresh gimp inkscape calibre klavaro kchmviewer geogebra freemind irssi hexchat thunderbird goldendict
 
 # multimedia
 arch-chroot /mnt pacman -Syu --needed --noconfirm obs-studio vlc kdenlive frei0r-plugins audacity aegisub kid3 kodi
