5786e741c48a2f98d30d1a64ad0dd5e874ca57c8
leanhtai01 <leanhtai01@gmail.com>
2020-07-31 14:14:18 +0700

    replace wget with curl install_yay.sh

--- post_install/normal_user_privilege/install_yay.sh
+++ post_install/normal_user_privilege/install_yay.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-wget https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
+curl -LJo yay.tar.gz https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
 tar -xvf yay.tar.gz
 cd yay/
 makepkg -sri --noconfirm
