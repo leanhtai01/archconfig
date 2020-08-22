fc09ca6b031e5272e3df00b59c61b967b50540c7
leanhtai01 <leanhtai01@gmail.com>
2020-08-22 09:45:54 +0700

    remove windows recovery partition when make partition for Windows 10

--- system_install/make_partitions_for_windows_10.sh
+++ system_install/make_partitions_for_windows_10.sh
@@ -22,9 +22,7 @@ fi
 
 # partition the disk
 sgdisk -Z /dev/$install_dev
-sgdisk -n 0:0:+1G -t 0:2700 -c 0:"win_re" /dev/$install_dev
 sgdisk -n 0:0:+1G -t 0:ef00 -c 0:"efi" /dev/$install_dev
 sgdisk -n 0:0:+1G -t 0:0c01 -c 0:"ms_reserved" /dev/$install_dev
 dd if=/dev/zero of=/dev/${install_dev}${part}1 bs=1M count=1
 dd if=/dev/zero of=/dev/${install_dev}${part}2 bs=1M count=1
-dd if=/dev/zero of=/dev/${install_dev}${part}3 bs=1M count=1
