194817cc5ccf051347f891a9336a632bfcfb66bf
leanhtai01 <leanhtai01@gmail.com>
2020-08-23 15:03:50 +0700

    add file make_partitions_for_windows_10_vm.sh

---
+++ tmp/make_partitions_for_windows_10_vm.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+# global variables
+install_dev=
+part=
+
+# let user choose device to make partitions
+printf "MAKE PARTITIONS FOR WINDOWS 10\n"
+if [ -z $install_dev ]
+then
+    lsblk
+    echo -n "Enter device to install: "
+    read install_dev
+fi
+
+if [ $install_dev = nvme0n1 ]
+then
+    part=p
+fi
+
+# partition the disk
+dd if=/dev/zero of=/dev/$install_dev bs=4M count=1
+sgdisk -Z /dev/$install_dev
+sgdisk -n 0:0:+1G -t 0:ef00 -c 0:"efi" /dev/$install_dev
+sgdisk -n 0:0:+1G -t 0:0c01 -c 0:"ms_reserved" /dev/$install_dev
+sgdisk -n 0:0:+1G -t 0:0700 -c 0:"ms_data" /dev/$install_dev
+dd if=/dev/zero of=/dev/${install_dev}${part}1 bs=4M count=1
+dd if=/dev/zero of=/dev/${install_dev}${part}2 bs=4M count=1
+dd if=/dev/zero of=/dev/${install_dev}${part}3 bs=4M count=1
+mkfs.fat -F32 /dev/${install_dev}${part}1
