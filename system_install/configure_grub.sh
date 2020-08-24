e30a1f8e3ae77b848f2e250be8142376641d423b
leanhtai01 <leanhtai01@gmail.com>
2020-08-24 09:07:29 +0700

    fix bug sed command configure GRUB background image

--- system_install/configure_grub.sh
+++ system_install/configure_grub.sh
@@ -58,7 +58,7 @@ fi
 mkdir /mnt/boot/grub/background
 cp $parent_dir/data/grubimage.png /mnt/boot/grub/background
 sed -i "/^#GRUB_BACKGROUND=/s/^#//" /mnt/etc/default/grub
-sed -i "/^GRUB_BACKGROUND=/s/\".*\"/\"/\"\/boot\/grub\/background\/grubimage.png\"/" /mnt/etc/default/grub
+sed -i "/^GRUB_BACKGROUND=/s/\".*\"/\"\/boot\/grub\/background\/grubimage.png\"/" /mnt/etc/default/grub
 
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Arch Linux" --recheck
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
