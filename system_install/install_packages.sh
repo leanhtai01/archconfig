1cec287178a1b178fb2cca53eaa64b41896a3fa6
leanhtai01 <leanhtai01@gmail.com>
2020-08-25 16:50:44 +0700

    add packages widelands, 0ad

--- system_install/install_packages.sh
+++ system_install/install_packages.sh
@@ -57,7 +57,7 @@ arch-chroot /mnt systemctl enable docker
 arch-chroot /mnt gpasswd -a $newusername docker
 
 # games
-arch-chroot /mnt pacman -Syu --needed --noconfirm kigo bovo gnuchess supertuxkart wesnoth minetest minetest-server teeworlds kbounce steam steam-native-runtime
+arch-chroot /mnt pacman -Syu --needed --noconfirm kigo bovo gnuchess supertuxkart wesnoth minetest minetest-server teeworlds kbounce steam steam-native-runtime widelands 0ad
 
 # wine
 arch-chroot /mnt pacman -Syu --needed --noconfirm wine-staging lutris playonlinux giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs winetricks
