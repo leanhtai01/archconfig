45f09c551c86cb704e07c05f6788b7ab6d71cbd7
leanhtai01 <leanhtai01@gmail.com>
2020-08-28 07:39:41 +0700

    make install packages scripts work outside arch-chroot

--- system_install/install_core_packages.sh
+++ system_install/install_core_packages.sh
@@ -2,55 +2,69 @@
 
 set -e
 
+prefix=
+
+if [ ! -z $1 ] && [ $1 = "in_chroot" ]
+then
+    prefix="arch-chroot /mnt "
+fi
+
+install_command="${prefix}pacman -Syu --needed --noconfirm"
+
 # install and configure some packages, services
 # system packages
-arch-chroot /mnt pacman -Syu --needed --noconfirm lm_sensors gparted dosfstools ntfs-3g p7zip unrar
+$install_command lm_sensors gparted dosfstools ntfs-3g p7zip unrar
 
 # fonts
-arch-chroot /mnt pacman -Syu --needed --noconfirm ttf-dejavu ttf-liberation adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-serif-cn-fonts adobe-source-han-serif-tw-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts noto-fonts-emoji ttf-hack
+$install_command ttf-dejavu ttf-liberation adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-serif-cn-fonts adobe-source-han-serif-tw-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts noto-fonts-emoji ttf-hack
 
 # audio
-arch-chroot /mnt pacman -Syu --needed --noconfirm pulseaudio-alsa alsa-utils pulseaudio-bluetooth
+$install_command pulseaudio-alsa alsa-utils pulseaudio-bluetooth
 
 # desktop environment
-arch-chroot /mnt pacman -Syu --needed --noconfirm xorg-server xorg-xrandr lib32-vulkan-icd-loader vulkan-icd-loader vulkan-intel lib32-vulkan-intel lib32-vkd3d vkd3d xf86-video-intel lib32-mesa mesa mesa-demos
+$install_command xorg-server xorg-xrandr lib32-vulkan-icd-loader vulkan-icd-loader vulkan-intel lib32-vulkan-intel lib32-vkd3d vkd3d xf86-video-intel lib32-mesa mesa mesa-demos
 
 # GNOME
-arch-chroot /mnt pacman -Syu --needed --noconfirm gnome gnome-extra seahorse seahorse-nautilus chrome-gnome-shell khelpcenter gnome-calendar
-arch-chroot /mnt systemctl enable gdm  
+$install_command gnome gnome-extra seahorse seahorse-nautilus chrome-gnome-shell khelpcenter gnome-calendar
+${prefix}systemctl enable gdm  
 
 # browsers
-arch-chroot /mnt pacman -Syu --needed --noconfirm chromium
+$install_command chromium
 
 # editors
-arch-chroot /mnt pacman -Syu --needed --noconfirm emacs
+$install_command emacs
 
 # programming packages
-arch-chroot /mnt pacman -Syu --needed --noconfirm gdb cmake git go
+$install_command gdb cmake git go
 
 # tools
-arch-chroot /mnt pacman -Syu --needed --noconfirm reflector wimlib transmission-gtk gnome-clocks alacarte file-roller
+$install_command reflector wimlib transmission-gtk gnome-clocks alacarte file-roller
 
 # office and learning
-arch-chroot /mnt pacman -Syu --needed --noconfirm libreoffice-fresh calibre kchmviewer goldendict kolourpaint thunderbird
+$install_command libreoffice-fresh calibre kchmviewer goldendict kolourpaint thunderbird
 
 # multimedia
-arch-chroot /mnt pacman -Syu --needed --noconfirm obs-studio vlc kdenlive frei0r-plugins
+$install_command obs-studio vlc kdenlive frei0r-plugins
 
 # virtualbox
-arch-chroot /mnt pacman -Syu --needed --noconfirm virtualbox virtualbox-guest-iso virtualbox-host-dkms
+$install_command virtualbox virtualbox-guest-iso virtualbox-host-dkms
 
 # wireshark
-arch-chroot /mnt pacman -Syu --needed --noconfirm wireshark-qt
-arch-chroot /mnt gpasswd -a $newusername wireshark
+$install_command wireshark-qt
 
 # docker
-arch-chroot /mnt pacman -Syu --needed --noconfirm docker
-arch-chroot /mnt systemctl enable docker
-arch-chroot /mnt gpasswd -a $newusername docker
+$install_command docker
+${prefix}systemctl enable docker
+
+# add user to group wireshark, docker
+if [ ! -z $1 ]
+then
+    ${prefix}gpasswd -a $1 wireshark
+    ${prefix}gpasswd -a $1 docker
+fi
 
 # games
-arch-chroot /mnt pacman -Syu --needed --noconfirm kigo bovo gnuchess
+$install_command kigo bovo gnuchess
 
 # enable bluetooth service
-arch-chroot /mnt systemctl enable bluetooth
+${prefix}systemctl enable bluetooth
