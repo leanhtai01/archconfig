9a0a085d476f5f3be472f4f33ab23af9c0a2e2f1
leanhtai01 <leanhtai01@gmail.com>
2020-08-30 08:19:58 +0700

    user just need enter name of USB

--- tools/make_bootable_usb_linux.sh
+++ tools/make_bootable_usb_linux.sh
@@ -3,15 +3,16 @@
 set -e
 
 path_to_iso=
-path_to_dev=
+device=
 
 lsblk
-read -e -p "Enter path to device: " path_to_dev
+read -e -p "Enter USB device (sda, sdb, sdX,...): " device
 read -e -p "Enter path to the iso: " path_to_iso
 gpg ${path_to_iso}.sig
 
-sudo dd if=/dev/zero of=$path_to_dev bs=4M count=1
-sudo sgdisk -Z $path_to_dev
-sudo sgdisk -n 0:0:0 $path_to_dev
-sudo dd if=$path_to_iso of=$path_to_dev bs=4M && sync
-udisksctl power-off -b $path_to_dev
+sudo dd if=/dev/zero of=/dev/$device bs=4M count=1
+sudo parted /dev/$device mklabel gpt
+sudo sgdisk -Z /dev/$device
+sudo sgdisk -n 0:0:0 /dev/$device
+sudo dd if=$path_to_iso of=/dev/$device bs=4M && sync
+udisksctl power-off -b /dev/$device
