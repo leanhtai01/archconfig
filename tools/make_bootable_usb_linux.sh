7730ea4c15ba71beca8c5c73e607690fdb0b4165
leanhtai01 <leanhtai01@gmail.com>
2020-09-01 09:24:52 +0700

    let user enter path to signature file

--- tools/make_bootable_usb_linux.sh
+++ tools/make_bootable_usb_linux.sh
@@ -3,12 +3,14 @@
 set -e
 
 path_to_iso=
+path_to_sig
 device=
 
 lsblk
 read -e -p "Enter USB device (sda, sdb, sdX,...): " device
 read -e -p "Enter path to the iso: " path_to_iso
-gpg ${path_to_iso}.sig
+read -e -p "Enter path to signature file: " path_to_sig
+gpg path_to_sig
 
 sudo dd if=/dev/zero of=/dev/$device bs=4M count=1
 sudo parted /dev/$device mklabel gpt
