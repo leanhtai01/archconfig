618e6f90d3f445facdae6c2bc2381f1031fabdd1
leanhtai01 <leanhtai01@gmail.com>
2020-09-01 12:27:49 +0700

    fix bug about files permission

--- tools/make_bootable_usb_windows.sh
+++ tools/make_bootable_usb_windows.sh
@@ -12,19 +12,29 @@ read -e -p "Enter path to the iso: " path_to_iso
 read -e -p "Enter path to hash file: " path_to_hash
 sha1sum -c $path_to_hash
 
+# format device
 sudo dd if=/dev/zero of=/dev/$device bs=4M count=1
 sudo parted /dev/$device mklabel gpt
 sudo sgdisk -Z /dev/$device
 sudo sgdisk -n 0:0:0 /dev/$device
 sudo mkfs.fat -F32 /dev/${device}1 -n "WINDOWS10"
-mkdir win_img tmp bootable_usb
+
+# mount the iso
+sudo mkdir win_img tmp bootable_usb
 sudo mount $path_to_iso win_img -o loop
-cp -r win_img/* tmp/
-wimsplit tmp/sources/install.wim tmp/sources/install.swm 2500
-printf "[Channel]\r\nRetail\r\n" > tmp/sources/ei.cfg
-rm tmp/sources/install.wim
+sudo cp -r win_img/* tmp/
+sudo chmod -R ugo+xrw tmp/*
+
+# split install.wim to fit fat32 filesystem
+sudo wimsplit tmp/sources/install.wim tmp/sources/install.swm 2500
+sudo rm tmp/sources/install.wim
+
+# let user choose version of Windows 10 on install
+sudo printf "[Channel]\r\nRetail\r\n" > tmp/sources/ei.cfg
+
+# copy install files to usb
 sudo mount /dev/${device}1 bootable_usb
-cp -r tmp/* bootable_usb/
+sudo cp -r tmp/* bootable_usb/
 sudo umount bootable_usb/ win_img/
 sudo rm -r tmp/ win_img/ bootable_usb/
 udisksctl power-off -b /dev/$device
