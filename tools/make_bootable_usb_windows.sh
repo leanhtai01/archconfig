5de33ef740354377dfbed48d266e807cb7af44cd
leanhtai01 <leanhtai01@gmail.com>
2020-09-01 10:45:13 +0700

    fix variables bug

--- tools/make_bootable_usb_windows.sh
+++ tools/make_bootable_usb_windows.sh
@@ -10,7 +10,7 @@ lsblk
 read -e -p "Enter USB device (sda, sdb, sdX,...): " device
 read -e -p "Enter path to the iso: " path_to_iso
 read -e -p "Enter path to hash file: " path_to_hash
-sha1sum -c path_to_hash
+sha1sum -c $path_to_hash
 
 sudo dd if=/dev/zero of=/dev/$device bs=4M count=1
 sudo parted /dev/$device mklabel gpt
@@ -18,7 +18,7 @@ sudo sgdisk -Z /dev/$device
 sudo sgdisk -n 0:0:0 /dev/$device
 sudo mkfs.fat -F32 /dev/${device}1 -n "WINDOWS10"
 sudo mkdir win_img tmp bootable_usb
-sudo mount path_to_iso win_img -o loop
+sudo mount $path_to_iso win_img -o loop
 sudo cp -r win_img/* tmp/
 sudo wimplit tmp/sources/install.wim tmp/sources/install.swm 2500
 sudo rm tmp/sources/install.wim
