7aac2d2f044d09db95655737c0169069aadc12f5
leanhtai01 <leanhtai01@gmail.com>
2020-09-02 18:47:48 +0700

    fix variable name syntax error

--- tools/make_bootable_usb_linux.sh
+++ tools/make_bootable_usb_linux.sh
@@ -10,7 +10,7 @@ lsblk
 read -e -p "Enter USB device (sda, sdb, sdX,...): " device
 read -e -p "Enter path to the iso: " path_to_iso
 read -e -p "Enter path to signature file: " path_to_sig
-gpg path_to_sig
+gpg $path_to_sig
 
 sudo dd if=/dev/zero of=/dev/$device bs=4M count=1
 sudo parted /dev/$device mklabel gpt
