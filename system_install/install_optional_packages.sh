5957a4a1db97df6a7136c2d1149da2399c5e4ef3
leanhtai01 <leanhtai01@gmail.com>
2020-09-08 09:29:30 +0700

    remove some packages

--- system_install/install_optional_packages.sh
+++ system_install/install_optional_packages.sh
@@ -27,10 +27,10 @@ $install_command remmina libvncserver freerdp
 $install_command librecad octave gimp inkscape geogebra sweethome3d
 
 # multimedia
-$install_command aegisub kid3 kodi digikam rhythmbox gst-libav blender handbrake handbrake-cli mkvtoolnix-gui pencil2d gnome-sound-recorder
+$install_command aegisub kid3 rhythmbox gst-libav blender handbrake handbrake-cli mkvtoolnix-gui pencil2d gnome-sound-recorder
 
 # games
-$install_command wesnoth steam steam-native-runtime discord
+$install_command wesnoth steam steam-native-runtime
 
 # wine
 $install_command wine-staging lutris playonlinux giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs winetricks
