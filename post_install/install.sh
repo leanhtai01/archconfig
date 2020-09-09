fe1898148fbdaf45ff550161f73d9d01b4b40e74
leanhtai01 <leanhtai01@gmail.com>
2020-09-09 09:12:02 +0700

    re-order post install files

--- post_install/install.sh
+++ post_install/install.sh
@@ -9,11 +9,10 @@ $current_dir/make_shortcuts_gnome.sh
 $current_dir/install_arch_cursor_theme.sh
 $current_dir/install_yay.sh
 $current_dir/configure_ibus.sh
-$current_dir/install_vmware_workstation.sh
-$current_dir/install_sublime_tools.sh
 $current_dir/configure_kvm.sh
 $current_dir/configure_lamp_stack.sh
 $current_dir/configure_tlp.sh
 $current_dir/configure_ufw.sh
-$current_dir/detect_other_os_grub.sh
 $current_dir/swap_caps_left_ctrl_console.sh
+$current_dir/install_vmware_workstation.sh
+$current_dir/install_sublime_tools.sh
