fe4703611da49d4ac961d8da015fee8d2edbc4da
leanhtai01 <leanhtai01@gmail.com>
2020-09-13 22:30:51 +0700

    remove remaining backup original config file process

--- post_install/configure_kvm.sh
+++ post_install/configure_kvm.sh
@@ -2,19 +2,10 @@
 
 set -e
 
-# make place to save original config files (if not exist)
-original_config_files_path=$(dirname $0)/original_config_files
-if [ ! -d "$original_config_files_path" ]
-then
-    mkdir $original_config_files_path
-fi
-
 sudo pacman -Syu --needed --noconfirm virt-manager qemu vde2 ebtables dnsmasq bridge-utils virt-viewer dmidecode
 sudo systemctl enable libvirtd
 sudo systemctl start libvirtd
 
-cp /etc/libvirt/libvirtd.conf $original_config_files_path
-printf "libvirtd.conf: /etc/libvirt/libvirtd.conf\n" >> $original_config_files_path/original_path.txt
 sudo sed -i "/^#unix_sock_group = \"libvirt\"$/s/^#//" /etc/libvirt/libvirtd.conf
 sudo sed -i "/^#unix_sock_rw_perms = \"0770\"$/s/^#//" /etc/libvirt/libvirtd.conf
 sudo gpasswd -a $(whoami) libvirt
--- post_install/swap_caps_left_ctrl_console.sh
+++ post_install/swap_caps_left_ctrl_console.sh
@@ -3,21 +3,12 @@
 # exit script immediately on error
 set -e
 
-# make place to save original config files (if not exist)
-original_config_files_path=$(dirname $0)/original_config_files
-if [ ! -d "$original_config_files_path" ]
-then
-    mkdir $original_config_files_path
-fi
-
 sudo mkdir -p /usr/local/share/kbd/keymaps
 sudo cp /usr/share/kbd/keymaps/i386/qwerty/us.map.gz /usr/local/share/kbd/keymaps/
 sudo gunzip /usr/local/share/kbd/keymaps/us.map.gz
 sudo mv /usr/local/share/kbd/keymaps/us.map /usr/local/share/kbd/keymaps/personal.map
 
 # make the Right Alt key same as Left Alt key
-cp /usr/local/share/kbd/keymaps/personal.map $original_config_files_path
-printf "personal.map: /usr/local/share/kbd/keymaps/personal.map\n" >> $original_config_files_path/original_path.txt
 linum=$(sed -n '/^include "linux-with-alt-and-altgr"$/=' /usr/local/share/kbd/keymaps/personal.map)
 sudo sed -i "${linum} a include \"linux-with-two-alt-keys\"" /usr/local/share/kbd/keymaps/personal.map
 
