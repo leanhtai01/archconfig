380cdba61c420901871808e2f2a1e0a05b94c7af
leanhtai01 <leanhtai01@gmail.com>
2020-09-13 22:42:07 +0700

    add user to group kvm

--- post_install/configure_kvm.sh
+++ post_install/configure_kvm.sh
@@ -9,3 +9,4 @@ sudo systemctl start libvirtd
 sudo sed -i "/^#unix_sock_group = \"libvirt\"$/s/^#//" /etc/libvirt/libvirtd.conf
 sudo sed -i "/^#unix_sock_rw_perms = \"0770\"$/s/^#//" /etc/libvirt/libvirtd.conf
 sudo gpasswd -a $(whoami) libvirt
+sudo gpasswd -a $(whoami) kvm
