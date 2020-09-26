d556a425f9a213f6b47d6b3ae9f09766743cd648
leanhtai01 <leanhtai01@gmail.com>
2020-09-26 12:02:51 +0700

    install visual studio code by default

--- post_install/install_packages.sh
+++ post_install/install_packages.sh
@@ -12,3 +12,4 @@ $current_dir/configure_tlp.sh
 $current_dir/configure_ufw.sh
 # $current_dir/install_vmware_workstation.sh
 $current_dir/install_sublime_tools.sh
+$current_dir/install_visual_studio_code.sh
