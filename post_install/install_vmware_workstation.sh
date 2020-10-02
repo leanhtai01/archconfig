1d96ebeadf52d05a88d3ce62e769159e11e3043f
leanhtai01 <leanhtai01@gmail.com>
2020-10-02 17:24:46 +0700

    change vmware key and not enable unavailable vmware service

--- post_install/install_vmware_workstation.sh
+++ post_install/install_vmware_workstation.sh
@@ -5,6 +5,5 @@ set -e
 yay -Syu --noconfirm vmware-workstation
 sudo systemctl enable vmware-networks
 sudo systemctl enable vmware-usbarbitrator
-sudo systemctl enable vmware-hostd
 sudo modprobe -a vmw_vmci vmmon
-sudo /usr/lib/vmware/bin/vmware-vmx-debug --new-sn FF788-A1X86-08E9Q-5YN79-XV0YD
+sudo /usr/lib/vmware/bin/vmware-vmx-debug --new-sn ZF3R0-FHED2-M80TY-8QYGC-NPKYF
