862dac8aed999827f21d2877b1beddfe40acd62c
leanhtai01 <leanhtai01@gmail.com>
2020-10-03 08:30:16 +0700

    replace dd with wipefs to clean device signature (dual-boot luks on lvm)

--- system_install/prepare_disk_dual-boot_with_windows_10_luks_on_lvm.sh
+++ system_install/prepare_disk_dual-boot_with_windows_10_luks_on_lvm.sh
@@ -10,7 +10,7 @@ lvm_partnum=5
 sgdisk -n 0:0:0 -t 0:8e00 -c 0:"lvm" /dev/$install_dev
 
 # create physical volume
-dd if=/dev/zero of=/dev/${install_dev}${part}${lvm_partnum} bs=4M count=1
+wipefs -a /dev/${install_dev}${part}${lvm_partnum}
 pvcreate /dev/${install_dev}${part}${lvm_partnum}
 
 # create volume group
@@ -18,11 +18,14 @@ vgcreate sys_vol_group /dev/${install_dev}${part}${lvm_partnum}
 
 # create logical volumes
 lvcreate -L `expr 2 \* $size_of_ram`G sys_vol_group -n cryptswap
+wipefs -a /dev/sys_vol_group/cryptswap
 lvcreate -l +100%FREE sys_vol_group -n cryptroot
+wipefs -a /dev/sys_vol_group/cryptroot
 
 # encrypt and format root partition
 printf "$storagepass1" | cryptsetup luksFormat --type luks2 /dev/sys_vol_group/cryptroot -
 printf "$storagepass1" | cryptsetup open /dev/sys_vol_group/cryptroot root -
+wipefs -a /dev/mapper/root
 
 # format the partition
 mkfs.ext4 /dev/mapper/root
