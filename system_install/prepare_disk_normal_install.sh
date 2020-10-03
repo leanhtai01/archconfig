879ee4bce5b766074ebb040db8d703dec7e30c21
leanhtai01 <leanhtai01@gmail.com>
2020-10-03 08:16:31 +0700

    replace dd with wipefs to clean device signature (normal install)

--- system_install/prepare_disk_normal_install.sh
+++ system_install/prepare_disk_normal_install.sh
@@ -3,7 +3,6 @@
 set -e
 
 # partition the disks
-dd if=/dev/zero of=/dev/$install_dev bs=4M count=1
 wipefs -a /dev/$install_dev
 sgdisk -Z /dev/$install_dev
 sgdisk -n 0:0:+1G -t 0:ef00 -c 0:"efi" /dev/$install_dev
@@ -11,9 +10,9 @@ sgdisk -n 0:0:+`expr 2 \* $size_of_ram`G -t 0:8200 -c 0:"swap" /dev/$install_dev
 sgdisk -n 0:0:0 -t 0:8304 -c 0:"root" /dev/$install_dev
 
 # format the partitions
-dd if=/dev/zero of=/dev/${install_dev}${part}1 bs=4M count=1
-dd if=/dev/zero of=/dev/${install_dev}${part}2 bs=4M count=1
-dd if=/dev/zero of=/dev/${install_dev}${part}3 bs=4M count=1
+wipefs -a /dev/${install_dev}${part}1
+wipefs -a /dev/${install_dev}${part}2
+wipefs -a /dev/${install_dev}${part}3
 mkfs.fat -F32 /dev/${install_dev}${part}1
 mkswap /dev/${install_dev}${part}2
 swapon /dev/${install_dev}${part}2
