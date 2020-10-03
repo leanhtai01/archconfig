4df50d93cdd320f7d8eac1ad8a579f01d8039d2f
leanhtai01 <leanhtai01@gmail.com>
2020-10-03 08:21:03 +0700

    replace dd with wipefs to clean device signature (luks on lvm)

--- system_install/prepare_disk_luks_on_lvm.sh
+++ system_install/prepare_disk_luks_on_lvm.sh
@@ -3,14 +3,13 @@
 set -e
 
 # partition the disk
-dd if=/dev/zero of=/dev/$install_dev bs=4M count=1
 wipefs -a /dev/$install_dev
 sgdisk -Z /dev/$install_dev
 sgdisk -n 0:0:+1G -t 0:ef00 -c 0:"efi" /dev/$install_dev
 sgdisk -n 0:0:0 -t 0:8e00 -c 0:"lvm" /dev/$install_dev
 
 # create physical volume
-dd if=/dev/zero of=/dev/${install_dev}${part}2 bs=4M count=1
+wipefs -a /dev/${install_dev}${part}2
 pvcreate /dev/${install_dev}${part}2
 
 # create volume group
@@ -18,14 +17,17 @@ vgcreate sys_vol_group /dev/${install_dev}${part}2
 
 # create logical volumes
 lvcreate -L `expr 2 \* $size_of_ram`G sys_vol_group -n cryptswap
+wipefs -a /dev/sys_vol_group/cryptswap
 lvcreate -l +100%FREE sys_vol_group -n cryptroot
+wipefs -a /dev/sys_vol_group/cryptroot
 
 # encrypt and format root partition
 printf "$storagepass1" | cryptsetup luksFormat --type luks2 /dev/sys_vol_group/cryptroot -
 printf "$storagepass1" | cryptsetup open /dev/sys_vol_group/cryptroot root -
+wipefs -a /dev/mapper/root
 
 # format the partitions
-dd if=/dev/zero of=/dev/${install_dev}${part}1 bs=4M count=1
+wipefs -a /dev/${install_dev}${part}1
 mkfs.fat -F32 /dev/${install_dev}${part}1
 mkfs.ext4 /dev/mapper/root
 
