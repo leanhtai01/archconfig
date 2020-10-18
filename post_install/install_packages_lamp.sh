368bea04af9ffc4a76a9ebad07a6aeeaed54576a
leanhtai01 <leanhtai01@gmail.com>
2020-10-18 08:04:45 +0700

    add file install_packages_lamp.sh

---
+++ post_install/install_packages_lamp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+# install Apache
+sudo pacman -Syu --needed --noconfirm apache
+sudo systemctl enable httpd
+sudo systemctl start httpd
+
+# install MariaDB
+sudo pacman -Syu --needed --noconfirm mariadb expect
+sudo mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+sudo systemctl enable mariadb
+sudo systemctl start mariadb
+
+# install PHP
+sudo pacman -Syu --needed --noconfirm php php-apache
+
+# install phpMyAdmin
+sudo pacman -Syu --needed --noconfirm phpmyadmin
