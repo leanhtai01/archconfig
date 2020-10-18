300d10848dc0dc0c35940b50a9f5c0f228e47013
leanhtai01 <leanhtai01@gmail.com>
2020-10-18 15:09:49 +0700

    add file install_i3wm.sh

---
+++ system_install/install_i3wm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+prefix=
+
+if [ ! -z $1 ] && [ $1 = "in_chroot" ]
+then
+    prefix="arch-chroot /mnt "
+fi
+
+install_command="${prefix}pacman -Syu --needed --noconfirm"
+
+$install_command i3-wm i3blocks i3lock i3status dmenu rxvt-unicode xorg-xinit
