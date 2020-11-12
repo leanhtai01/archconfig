e7bd0fb0dc3012fc7f1f691e8226cda5e838a238
leanhtai01 <leanhtai01@gmail.com>
2020-11-12 09:44:44 +0700

    add option to install KDE Plasma DE

---
+++ system_install/install_KDE_Plasma.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+prefix=
+
+if [ ! -z $1 ] && [ $1 = "in_chroot" ]
+then
+    prefix="arch-chroot /mnt "
+fi
+
+install_command="${prefix}pacman -Syu --needed --noconfirm"
+
+$install_command plasma ark dolphin dolphin-plugins kate kleopatra konsole okular kdegraphics-thumbnailers digikam ffmpegthumbs spectacle gwenview
+${prefix}systemctl enable sddm
--- system_install/install_desktop_environment.sh
+++ system_install/install_desktop_environment.sh
@@ -8,6 +8,10 @@ case $1 in
     GNOME)
 	. $current_dir/install_GNOME.sh $2
 	;;
+    KDEPlasma)
+	. $current_dir/install_KDE_Plasma.sh $2
+	;;
     i3)
 	. $current_dir/install_i3wm.sh $2
+	;;
 esac
