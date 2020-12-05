6b026404eaa1ab62b94991e8c52abf363f736580
leanhtai01 <leanhtai01@gmail.com>
2020-12-05 08:42:11 +0700

    post install vmware by default

--- post_install/install_packages.sh
+++ post_install/install_packages.sh
@@ -10,7 +10,7 @@ $current_dir/configure_kvm.sh
 $current_dir/install_lamp_stack.sh
 $current_dir/configure_tlp.sh
 $current_dir/configure_ufw.sh
-# $current_dir/install_vmware_workstation.sh
+$current_dir/install_vmware_workstation.sh
 $current_dir/install_sublime_tools.sh
 $current_dir/install_visual_studio_code.sh
 $current_dir/install_usb_wifi_driver.sh
