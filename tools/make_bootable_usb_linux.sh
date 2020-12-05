fd3d88f38e9089f41faffa95cddf9877ebe8dfb5
leanhtai01 <leanhtai01@gmail.com>
2020-12-05 09:07:40 +0700

    clean disk before make bootable usb

--- tools/make_bootable_usb_linux.sh
+++ tools/make_bootable_usb_linux.sh
@@ -4,13 +4,13 @@ set -e
 
 path_to_iso=
 device=
+current_dir=$(dirname $0)
 
 lsblk
 read -e -p "Enter USB device (sda, sdb, sdX,...): " device
 read -e -p "Enter path to the iso: " path_to_iso
 
-sudo wipefs -a /dev/$device
-sudo sgdisk -Z /dev/$device
+$current_dir/clean_disk.sh $device
 sudo sgdisk -n 0:0:0 /dev/$device
 sudo dd if=$path_to_iso of=/dev/$device bs=4M conv=sync
 sleep 30
--- tools/make_bootable_usb_windows.sh
+++ tools/make_bootable_usb_windows.sh
@@ -4,14 +4,14 @@ set -e
 
 path_to_iso=
 device=
+current_dir=$(dirname $0)
 
 lsblk
 read -e -p "Enter USB device (sda, sdb, sdX,...): " device
 read -e -p "Enter path to the iso: " path_to_iso
 
 # format device
-sudo wipefs -a /dev/$device
-sudo sgdisk -Z /dev/$device
+$current_dir/clean_disk.sh $device
 sudo sgdisk -n 0:0:0 /dev/$device
 sudo mkfs.fat -F32 /dev/${device}1 -n "WINDOWS10"
 
