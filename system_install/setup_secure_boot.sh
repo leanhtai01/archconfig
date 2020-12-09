2750eebc1548e80e124538b0e8a5d9be7bbdc9c7
leanhtai01 <leanhtai01@gmail.com>
2020-12-09 13:46:49 +0700

    separate setup secure boot and setup KeyTool

--- system_install/setup_secure_boot.sh
+++ system_install/setup_secure_boot.sh
@@ -11,8 +11,6 @@ case $bootloader in
     1) # systemd-boot
         cp $parent_dir/data/preloader-signed/{PreLoader,HashTool}.efi /mnt/boot/EFI/systemd
         cp /mnt/boot/EFI/systemd/systemd-bootx64.efi /mnt/boot/EFI/systemd/loader.efi
-	arch-chroot /mnt cp /usr/share/efitools/efi/KeyTool.efi /boot/EFI/systemd/
-	arch-chroot /mnt efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "KeyTool" --loader /EFI/systemd/KeyTool.efi
 	arch-chroot /mnt efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "Arch Linux SB" --loader /EFI/systemd/PreLoader.efi
 	;;
     2) # GRUB (encrypted boot)
@@ -20,8 +18,6 @@ case $bootloader in
     3) # GRUB (non-encrypted boot)
         cp $parent_dir/data/preloader-signed/{PreLoader,HashTool}.efi /mnt/boot/efi/EFI/"Arch Linux"
         cp /mnt/boot/efi/EFI/"Arch Linux"/grubx64.efi /mnt/boot/efi/EFI/"Arch Linux"/loader.efi
-	arch-chroot /mnt cp /usr/share/efitools/efi/KeyTool.efi /boot/efi/EFI/"Arch Linux"/
-	arch-chroot /mnt efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "KeyTool" --loader /EFI/"Arch Linux"/KeyTool.efi
 	arch-chroot /mnt efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "Arch Linux SB" --loader /EFI/"Arch Linux"/PreLoader.efi
 	;;
 esac
