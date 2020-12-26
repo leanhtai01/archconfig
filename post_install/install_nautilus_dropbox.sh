2ef7c60eac49735463576e4e96671d531d33a940
leanhtai01 <leanhtai01@gmail.com>
2020-12-26 13:51:08 +0700

    remove unused dependencies after install nautilus-dropbox

--- post_install/install_nautilus_dropbox.sh
+++ post_install/install_nautilus_dropbox.sh
@@ -4,3 +4,6 @@ set -e
 
 gpg --keyserver keys.gnupg.net --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
 yay -Syu --noconfirm nautilus-dropbox
+
+# remove unused dependencies
+pacman -Qdtq | sudo pacman --noconfirm -Rs -
