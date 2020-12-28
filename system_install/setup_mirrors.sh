f39d4abdfef9b607f5c24ddc350dd3e48b9d7631
leanhtai01 <leanhtai01@gmail.com>
2020-12-28 13:03:43 +0700

    remove www from URL download packages setup mirrors

--- system_install/setup_mirrors.sh
+++ system_install/setup_mirrors.sh
@@ -7,9 +7,9 @@ parent_dir=$(cd $(dirname $0)/..; pwd)
 
 mkdir pkg
 
-# curl -LJo pkg/reflector.pkg.tar.zst https://www.archlinux.org/packages/community/any/reflector/download/
-curl -LJo pkg/fakeroot.pkg.tar.zst https://www.archlinux.org/packages/core/x86_64/fakeroot/download/
-curl -LJo pkg/pacman-contrib.pkg.tar.zst https://www.archlinux.org/packages/community/x86_64/pacman-contrib/download/
+# curl -LJo pkg/reflector.pkg.tar.zst https://archlinux.org/packages/community/any/reflector/download/
+curl -LJo pkg/fakeroot.pkg.tar.zst https://archlinux.org/packages/core/x86_64/fakeroot/download/
+curl -LJo pkg/pacman-contrib.pkg.tar.zst https://archlinux.org/packages/community/x86_64/pacman-contrib/download/
 # pacman -U --noconfirm pkg/reflector.pkg.tar.zst
 pacman -U --noconfirm pkg/fakeroot.pkg.tar.zst
 pacman -U --noconfirm pkg/pacman-contrib.pkg.tar.zst
