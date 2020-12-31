49ca92d1b76584ec94fe948a9fc2b4d6e15f4e1f
leanhtai01 <leanhtai01@gmail.com>
2020-12-31 11:06:31 +0700

    move cockpit install to post install: install along with KVM

--- post_install/configure_kvm.sh
+++ post_install/configure_kvm.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-sudo pacman -Syu --needed --noconfirm virt-manager qemu vde2 ebtables dnsmasq bridge-utils virt-viewer dmidecode edk2-ovmf
+sudo pacman -Syu --needed --noconfirm virt-manager qemu vde2 ebtables dnsmasq bridge-utils virt-viewer dmidecode edk2-ovmf cockpit cockpit-machines
 sudo systemctl enable libvirtd
 sudo systemctl start libvirtd
+sudo systemctl enable cockpit.socket
+sudo systemctl start cockpit.socket
 
 sudo sed -i "/^#unix_sock_group = \"libvirt\"$/s/^#//" /etc/libvirt/libvirtd.conf
 sudo sed -i "/^#unix_sock_rw_perms = \"0770\"$/s/^#//" /etc/libvirt/libvirtd.conf
