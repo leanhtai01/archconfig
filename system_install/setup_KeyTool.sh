7112c7d35507137bcee5f53a746e7c27ef9ff71d
leanhtai01 <leanhtai01@gmail.com>
2021-01-02 10:52:53 +0700

    create EFIs dir in actual system

--- system_install/setup_KeyTool.sh
+++ system_install/setup_KeyTool.sh
@@ -9,14 +9,14 @@ arch-chroot /mnt pacman -Syu --needed --noconfirm efitools
 
 case $bootloader in
     1) # systemd-boot
-	mkdir -p /boot/EFI/systemd/ # create dir if not exist
+	arch-chroot /mnt mkdir -p /boot/EFI/systemd/ # create dir if not exist
 	arch-chroot /mnt cp /usr/share/efitools/efi/KeyTool.efi /boot/EFI/systemd/
 	arch-chroot /mnt efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "KeyTool" --loader /EFI/systemd/KeyTool.efi
 	;;
     2) # GRUB (encrypted boot)
 	;&
     3) # GRUB (non-encrypted boot)
-	mkdir -p /boot/efi/EFI/"Arch Linux"/
+	arch-chroot /mnt mkdir -p /boot/efi/EFI/"Arch Linux"/
 	arch-chroot /mnt cp /usr/share/efitools/efi/KeyTool.efi /boot/efi/EFI/"Arch Linux"/
 	arch-chroot /mnt efibootmgr --verbose --disk /dev/$install_dev --part $partnum --create --label "KeyTool" --loader /EFI/"Arch Linux"/KeyTool.efi
 	;;
