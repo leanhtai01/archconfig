a77712210b0c3227a016b2f2fd12a082e6f429f6
leanhtai01 <leanhtai01@gmail.com>
2021-01-06 22:16:21 +0700

    post install: not configure - install some packages

--- post_install/install_packages.sh
+++ post_install/install_packages.sh
@@ -10,14 +10,14 @@ $current_dir/configure_kvm.sh
 $current_dir/install_lamp_stack.sh
 $current_dir/configure_tlp.sh
 $current_dir/configure_ufw.sh
-$current_dir/install_vmware_workstation.sh
+# $current_dir/install_vmware_workstation.sh
 $current_dir/install_sublime_tools.sh
 $current_dir/install_mssql.sh
 $current_dir/install_visual_studio_code.sh
-$current_dir/install_azure_data_studio.sh
+# $current_dir/install_azure_data_studio.sh
 # $current_dir/install_nautilus_dropbox.sh
 $current_dir/install_usb_wifi_driver.sh
 $current_dir/install_virtualbox-ext-oracle.sh
 $current_dir/install_zoom.sh
 $current_dir/install_roslynpad.sh
-$current_dir/install_minecraft.sh
+# $current_dir/install_minecraft.sh
