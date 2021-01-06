f18ea3357d9e1902d1d2d20c0619a83b24487f6f
leanhtai01 <leanhtai01@gmail.com>
2021-01-06 20:56:35 +0700

    configure redshift

--- system_install/install_i3wm.sh
+++ system_install/install_i3wm.sh
@@ -15,13 +15,19 @@ install_command="${prefix}pacman -Syu --needed --noconfirm"
 $install_command i3-wm i3lock i3status dmenu rxvt-unicode xorg-xinit feh xss-lock xorg-xinput network-manager-applet geoclue redshift
 
 # configure i3
+# get/create required files and dirs
 ${prefix}mkdir -p /home/${newusername}/git_repos/archconfig
 ${prefix}mkdir -p /home/${newusername}/git_repos/i3config
 ${prefix}git clone https://github.com/leanhtai01/archconfig /home/${newusername}/git_repos/archconfig
 ${prefix}git clone https://github.com/leanhtai01/i3config /home/${newusername}/git_repos/i3config
 ${prefix}mkdir -p /home/${newusername}/.config
+
+# copy required files
 ${prefix}cp -r /home/${newusername}/git_repos/i3config/i3 /home/${newusername}/.config
 ${prefix}cp -r /home/${newusername}/git_repos/i3config/i3status /home/${newusername}/.config
+${prefix}cp -r /home/${newusername}/git_repos/i3config/redshift /home/${newusername}/.config
 ${prefix}cp /home/${newusername}/git_repos/archconfig/data/tiling_window_manager/.Xresources /home/${newusername}
+
+# change owner from root to user
 ${prefix}chown -R ${newusername}:${newusername} /home/${newusername}/git_repos
 ${prefix}chown -R ${newusername}:${newusername} /home/${newusername}/.config
