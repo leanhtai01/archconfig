bec086ddafc789bee37bd7d62de963a382ad0d8c
leanhtai01 <leanhtai01@gmail.com>
2021-01-07 16:05:21 +0700

    install all optional packages by default

--- system_install/install_core_packages.sh
+++ system_install/install_core_packages.sh
@@ -19,23 +19,23 @@ $install_command ttf-dejavu ttf-liberation noto-fonts-emoji ttf-hack adobe-sourc
 $install_command pulseaudio-alsa alsa-utils pulseaudio-bluetooth
 
 # usb 3g modem
-# $install_command modemmanager usb_modeswitch wvdial
-# ${prefix}systemctl enable ModemManager
+$install_command modemmanager usb_modeswitch wvdial
+${prefix}systemctl enable ModemManager
 
 # browsers
 $install_command chromium torbrowser-launcher
-# $install_command firefox-developer-edition
+$install_command firefox-developer-edition
 
 # editors
 $install_command emacs gvim
 
 # programming packages
 $install_command gdb cmake git go valgrind tk dia clang gcc python dotnet-sdk nodejs eslint npm
-# $install_command intellij-idea-community-edition pycharm-community-edition netbeans jdk-openjdk # optional
+$install_command intellij-idea-community-edition pycharm-community-edition netbeans jdk-openjdk # optional
 
 # tools
 $install_command wimlib transmission-gtk keepassxc expect pacman-contrib deja-dup curl kdiff3 lm_sensors dosfstools ntfs-3g p7zip unrar gparted wget bash-completion aircrack-ng nautilus gnome-calculator gnome-disk-utility
-# $install_command reflector cdrtools fuseiso gprename pdftk fish youtube-dl efitools sbsigntools gnome-clocks # optional
+$install_command reflector cdrtools fuseiso gprename pdftk fish youtube-dl efitools sbsigntools gnome-clocks # optional
 
 # install documentation for KDE Applications
 $install_command khelpcenter
@@ -49,11 +49,11 @@ $install_command remmina libvncserver freerdp
 
 # office and learning
 $install_command calibre kchmviewer goldendict kolourpaint evince thunderbird
-# $install_command libreoffice-fresh librecad gimp inkscape geogebra sweethome3d hexchat gnucash # optional
+$install_command libreoffice-fresh librecad gimp inkscape geogebra sweethome3d hexchat gnucash # optional
 
 # multimedia
 $install_command obs-studio vlc kdenlive frei0r-plugins
-# $install_command blender handbrake handbrake-cli audacity aegisub kid3 rhythmbox mkvtoolnix-gui pencil2d # optional
+$install_command blender handbrake handbrake-cli audacity aegisub kid3 rhythmbox mkvtoolnix-gui pencil2d # optional
 
 # virtualbox
 $install_command virtualbox virtualbox-guest-iso virtualbox-host-dkms
@@ -70,16 +70,16 @@ then
 fi
 
 # docker
-# $install_command docker docker-compose
-# ${prefix}systemctl enable docker
-# if [ ! -z $1 ]
-# then
-#     ${prefix}gpasswd -a $1 docker
-# fi
+$install_command docker docker-compose
+${prefix}systemctl enable docker
+if [ ! -z $1 ]
+then
+    ${prefix}gpasswd -a $1 docker
+fi
 
 # games
 $install_command steam steam-native-runtime 
-# $install_command discord kigo bovo gnuchess wesnoth # optional
+$install_command discord kigo bovo gnuchess wesnoth # optional
 
 # enable bluetooth service
 ${prefix}systemctl enable bluetooth
