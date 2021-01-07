1228612812932771858aa8c8afa9c758148e5570
leanhtai01 <leanhtai01@gmail.com>
2021-01-07 16:03:06 +0700

    install GNOME, KDE Plasma, i3 by default

--- system_install/install_base_system.sh
+++ system_install/install_base_system.sh
@@ -24,7 +24,7 @@ bootloader=1 # 1 - systemd-boot, 2 - GRUB
 swapuuidvalue=
 setupsecureboot=n
 setupkeytool=y
-desktop_environment="i3" # {GNOME, KDEPlasma, i3, none}
+desktop_environment="GNOME KDEPlasma i3" # {GNOME, KDEPlasma, i3, none}
 gpu=intel # {intel, amd, nvidia, virtualbox, vmware}
 
 # get user info
