697a3970433833fc57d512ac0a29ad4d74eff9d9
leanhtai01 <leanhtai01@gmail.com>
2021-01-10 20:54:24 +0700

    accept command line arguments when making bootable usb linux

--- tools/make_bootable_usb_linux.sh
+++ tools/make_bootable_usb_linux.sh
@@ -2,13 +2,22 @@
 
 set -e
 
-path_to_iso=
-device=
+dev=$1
+path_to_iso=$2
 current_dir=$(dirname $0)
 
-lsblk
-read -e -p "Enter USB device (sda, sdb, sdX,...): " device
-read -e -p "Enter path to the iso: " path_to_iso
+# let user choose device
+if [ -z $dev ]
+then
+    lsblk
+    read -e -p "Enter USB device (sda, sdb, sdX,...): " dev
+fi
+
+# let user choose iso file
+if [ -z $path_to_iso ]
+then
+    read -e -p "Enter path to the iso: " path_to_iso
+fi
 
 $current_dir/clean_disk.sh $device
 sudo sgdisk -n 0:0:0 /dev/$device
@@ -17,4 +26,4 @@ sleep 30
 lsblk
 udisksctl power-off -b /dev/$device
 lsblk
-printf "Success!\n"
+printf "Successfully write ${path_to_iso} to ${dev}!\n"
