1678e870e5c863e5b87cb1c49149e4c03faac360
leanhtai01 <leanhtai01@gmail.com>
2021-01-10 22:41:34 +0700

    fix variable's name

--- tools/make_bootable_usb_linux.sh
+++ tools/make_bootable_usb_linux.sh
@@ -19,11 +19,11 @@ then
     read -e -p "Enter path to the iso: " path_to_iso
 fi
 
-$current_dir/clean_disk.sh $device
-sudo sgdisk -n 0:0:0 /dev/$device
-sudo dd if=$path_to_iso of=/dev/$device bs=4M conv=sync
+$current_dir/clean_disk.sh $dev
+sudo sgdisk -n 0:0:0 /dev/$dev
+sudo dd if=$path_to_iso of=/dev/$dev bs=4M conv=sync
 sleep 30
 lsblk
-udisksctl power-off -b /dev/$device
+udisksctl power-off -b /dev/$dev
 lsblk
 printf "Successfully write ${path_to_iso} to ${dev}!\n"
