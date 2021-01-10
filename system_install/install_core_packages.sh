130a288af60743c25ca825058b99235a517d668b
leanhtai01 <leanhtai01@gmail.com>
2021-01-10 21:28:44 +0700

    install kdevelop, qtcreator and theirs all optional dependencies

--- system_install/install_core_packages.sh
+++ system_install/install_core_packages.sh
@@ -33,6 +33,14 @@ $install_command emacs gvim
 $install_command gdb cmake git go valgrind tk dia clang gcc python dotnet-sdk nodejs eslint npm
 $install_command intellij-idea-community-edition pycharm-community-edition netbeans jdk-openjdk # optional
 
+# install kdevelop and its all optional dependencies
+$install_command kdevelop
+$install_command $(printf "$(${prefix}pacman -Qi kdevelop)" | sed -n '/^Optional Deps/,$p' | sed '/^Required By/q' | head -n -1 | cut -c19- | cut -d: -f1)
+
+# install qtcreator and its all optional dependencies
+$install_command qtcreator
+$install_command $(printf "$(${prefix}pacman -Qi qtcreator)" | sed -n '/^Optional Deps/,$p' | sed '/^Required By/q' | head -n -1 | cut -c19- | cut -d: -f1)
+
 # tools
 $install_command wimlib transmission-gtk keepassxc expect pacman-contrib deja-dup curl kdiff3 lm_sensors dosfstools ntfs-3g p7zip unrar gparted wget bash-completion aircrack-ng nautilus gnome-calculator gnome-disk-utility
 $install_command reflector cdrtools fuseiso gprename pdftk fish youtube-dl efitools sbsigntools gnome-clocks # optional
