8a2455fbc08a7a334a2c2a32617d5d39ee22179d
leanhtai01 <leanhtai01@gmail.com>
2021-01-25 20:39:25 +0700

    install GNOME by default

--- system_install/install_GNOME.sh
+++ system_install/install_GNOME.sh
@@ -12,4 +12,4 @@ fi
 install_command="${prefix}pacman -Syu --needed --noconfirm"
 
 $install_command baobab cheese eog evince file-roller gdm gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-control-center gnome-disk-utility gnome-keyring gnome-logs gnome-menus gnome-photos gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra mutter nautilus sushi seahorse seahorse-nautilus chrome-gnome-shell khelpcenter dconf-editor gnome-tweaks totem evolution evolution-ews alacarte
-# ${prefix}systemctl enable gdm
+${prefix}systemctl enable gdm
