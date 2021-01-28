53f5f17544a5ed3838fb39f29fe887da2b1c7ae5
leanhtai01 <leanhtai01@gmail.com>
2021-01-28 14:07:12 +0700

    add some devel packages for GNOME

--- system_install/install_GNOME.sh
+++ system_install/install_GNOME.sh
@@ -11,5 +11,6 @@ fi
 
 install_command="${prefix}pacman -Syu --needed --noconfirm"
 
-$install_command baobab cheese eog evince file-roller gdm gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-control-center gnome-disk-utility gnome-keyring gnome-logs gnome-menus gnome-photos gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra mutter nautilus sushi seahorse seahorse-nautilus chrome-gnome-shell dconf-editor gnome-tweaks totem alacarte
+$install_command baobab cheese eog evince file-roller gdm gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-control-center gnome-disk-utility gnome-keyring gnome-logs gnome-menus gnome-photos gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra mutter nautilus sushi seahorse seahorse-nautilus chrome-gnome-shell dconf-editor gnome-tweaks totem alacarte gedit gedit-plugins
+$install_command devhelp glade gnome-builder gnome-devel-docs gnome-code-assistance
 ${prefix}systemctl enable gdm
