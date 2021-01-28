633fdc2647da9522c66e5a7e8a46f0b65c7ff232
leanhtai01 <leanhtai01@gmail.com>
2021-01-28 14:53:59 +0700

    post install: configure gedit

---
+++ post_install/configure_gedit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# display right margin
+gsettings set org.gnome.gedit.preferences.editor display-right-margin true
+
+# insert spaces
+gsettings set org.gnome.gedit.preferences.editor insert-spaces true
+
+# set theme
+gsettings set org.gnome.gedit.preferences.editor scheme "'builder-dark'"
+
+# enable plugins
+gsettings set org.gnome.gedit.plugins active-plugins "['codecomment', 'colorpicker', 'wordcompletion', 'commander', 'bracketcompletion', 'smartspaces', 'spell', 'devhelp', 'sessionsaver', 'git', 'terminal', 'sort', 'filebrowser', 'modelines', 'docinfo', 'quickhighlight', 'multiedit', 'drawspaces', 'bookmarks', 'quickopen', 'findinfiles', 'externaltools']"
