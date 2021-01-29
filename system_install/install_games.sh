8c901785c45c0c63063b0dd9639c9507df834a6e
leanhtai01 <leanhtai01@gmail.com>
2021-01-29 09:42:46 +0700

    add file install_games.sh

---
+++ system_install/install_games.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+prefix=
+
+if [ ! -z $1 ] && [ $1 = "in_chroot" ]
+then
+    prefix="arch-chroot /mnt "
+fi
+
+install_command="${prefix}pacman -Syu --needed --noconfirm"
+
+# install lutris and its all optional dependencies
+$install_command lutris
+$install_command $(printf "$(${prefix}pacman -Qi lutris)" | sed -n '/^Optional Deps/,$p' | sed '/^Required By/q' | head -n -1 | cut -c19- | cut -d: -f1)
+
+# install wine and its all dependencies
+$install_command wine
+$install_command $(printf "$(${prefix}pacman -Qi wine)" | sed -n '/^Optional Deps/,$p' | sed '/^Required By/q' | head -n -1 | cut -c19- | cut -d: -f1)
+
+# install steam
+$install_command steam steam-native-runtime
