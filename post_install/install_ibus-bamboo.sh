a482ad085b49fa8d3dea9b3681aac1b9e63e0744
leanhtai01 <leanhtai01@gmail.com>
2021-01-29 16:21:14 +0700

    setup input for GNOME after install ibus-bamboo

--- post_install/install_ibus-bamboo.sh
+++ post_install/install_ibus-bamboo.sh
@@ -6,4 +6,9 @@ sudo pacman -Syu --needed --noconfirm ibus
 mkdir -p /home/$(whoami)/git_repos/ibus-bamboo
 git clone https://github.com/BambooEngine/ibus-bamboo /home/$(whoami)/git_repos/ibus-bamboo
 (cd /home/$(whoami)/git_repos/ibus-bamboo && sudo make install)
+
+ibus-daemon -drx
 ibus restart
+
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
+gsettings set org.gnome.desktop.interface gtk-im-module "'ibus'"
