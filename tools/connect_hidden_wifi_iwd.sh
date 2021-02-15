165d30b3321e4373eca30d50e879b3dfbb52bea4
leanhtai01 <leanhtai01@gmail.com>
2021-02-15 13:18:12 +0700

    add file connect_hidden_wifi_iwd.sh

---
+++ tools/connect_hidden_wifi_iwd.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+PASSPHRASE=
+DEVICE=
+SSID=
+
+if [ -z $PASSPHRASE ]
+then
+    read -e -p "Enter passphrase: " PASSPHRASE
+fi
+
+if [ -z $DEVICE ]
+then
+    iwctl device list
+    read -e -p "Enter device: " DEVICE
+fi
+
+if [ -z $SSID ]
+then
+    iwctl station "$DEVICE" scan
+    iwctl station "$DEVICE" get-hidden-access-points
+    iwctl station "$DEVICE" get-networks
+fi
+
+iwctl device list
+iwctl --passphrase="$PASSPHRASE" station "$DEVICE" connect-hidden "$SSID"
