8ef80be6c3895092bbb621b3b96dff173d09ad3e
leanhtai01 <leanhtai01@gmail.com>
2021-02-15 13:31:18 +0700

    change keyserver for Dropbox gpg key

--- post_install/install_nautilus_dropbox.sh
+++ post_install/install_nautilus_dropbox.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-gpg --keyserver keys.gnupg.net --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
+gpg --keyserver keyserver.ubuntu.com --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
 yay -Syu --noconfirm nautilus-dropbox
 
 # remove unused dependencies
