40c2f8cb9755616b61bd515016fc0e74c31b15d7
leanhtai01 <leanhtai01@gmail.com>
2021-02-20 07:29:35 +0700

    prevent /etc/resolv.conf from modified

--- post_install/install_dnscrypt-proxy.sh
+++ post_install/install_dnscrypt-proxy.sh
@@ -10,6 +10,9 @@ sudo printf "nameserver ::1\n" > /etc/resolv.conf
 sudo printf "nameserver 127.0.0.1\n" >> /etc/resolv.conf
 sudo printf "options edns0 single-request-reopen\n" >> /etc/resolv.conf
 
+# prevent /etc/resolv.conf from modified
+sudo chattr +i /etc/resolv.conf
+
 # start systemd service
 sudo systemctl enable dnscrypt-proxy.service
 sudo systemctl start dnscrypt-proxy.service
