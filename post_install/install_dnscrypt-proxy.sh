a99c968a7239dffebc46f6703e6d61a4654fe48c
leanhtai01 <leanhtai01@gmail.com>
2021-02-20 08:06:06 +0700

    fix bug: configure dnscrypt-proxy failed

--- post_install/install_dnscrypt-proxy.sh
+++ post_install/install_dnscrypt-proxy.sh
@@ -6,9 +6,9 @@ sudo pacman -Syu --needed --noconfirm dnscrypt-proxy
 sudo cp /etc/resolv.conf /etc/resolv.conf.bak
 
 # modify /etc/resolv.conf
-sudo printf "nameserver ::1\n" > /etc/resolv.conf
-sudo printf "nameserver 127.0.0.1\n" >> /etc/resolv.conf
-sudo printf "options edns0 single-request-reopen\n" >> /etc/resolv.conf
+printf "nameserver ::1\n" | sudo tee /etc/resolv.conf
+printf "nameserver 127.0.0.1\n" | sudo tee -a /etc/resolv.conf
+printf "options edns0 single-request-reopen\n" | sudo tee -a /etc/resolv.conf
 
 # prevent /etc/resolv.conf from modified
 sudo chattr +i /etc/resolv.conf
@@ -16,3 +16,6 @@ sudo chattr +i /etc/resolv.conf
 # start systemd service
 sudo systemctl enable dnscrypt-proxy.service
 sudo systemctl start dnscrypt-proxy.service
+
+# restart Network Manager
+systemctl restart NetworkManager
