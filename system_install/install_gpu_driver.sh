ca2dbba93e640cd9811f5e64179d2f81e683cda3
leanhtai01 <leanhtai01@gmail.com>
2021-02-21 12:23:21 +0700

    add package libva-utils

--- system_install/install_gpu_driver.sh
+++ system_install/install_gpu_driver.sh
@@ -14,7 +14,7 @@ install_command="${prefix}pacman -Syu --needed --noconfirm"
 # driver installation
 case $1 in
     intel)
-	$install_command lib32-vulkan-icd-loader vulkan-icd-loader vulkan-intel intel-media-driver lib32-vulkan-intel lib32-mesa mesa ocl-icd lib32-ocl-icd intel-compute-runtime
+	$install_command lib32-vulkan-icd-loader vulkan-icd-loader vulkan-intel intel-media-driver lib32-vulkan-intel lib32-mesa mesa ocl-icd lib32-ocl-icd intel-compute-runtime libva-utils
 	;;
     amd)
 	;;
