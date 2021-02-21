62b7525383d23f3a6de04f3ba0d63b742b6a5214
leanhtai01 <leanhtai01@gmail.com>
2021-02-21 11:46:47 +0700

    shortcut: replace chromium with google-chrome-stable

--- post_install/make_shortcuts_gnome.sh
+++ post_install/make_shortcuts_gnome.sh
@@ -19,9 +19,9 @@ gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}1/ binding "'<Primary><Alt>f'"
 gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}1/ command "'krusader'"
 
 # open Chromium
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ name "'Chromium'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ name "'Google Chrome'"
 gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ binding "'<Primary><Alt>i'"
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ command "'chromium'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ command "'google-chrome-stable'"
 
 # open GNOME Terminal
 gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}3/ name "'GNOME Terminal'"
