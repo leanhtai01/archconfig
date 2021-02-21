1d2c65678f590946fe426ed820d9d70c41b4204b
leanhtai01 <leanhtai01@gmail.com>
2021-02-21 13:03:44 +0700

    shortcut: replace Google Chrome with Firefox Developer Edition

--- post_install/make_shortcuts_gnome.sh
+++ post_install/make_shortcuts_gnome.sh
@@ -19,9 +19,9 @@ gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}1/ binding "'<Primary><Alt>f'"
 gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}1/ command "'krusader'"
 
 # open Chromium
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ name "'Google Chrome'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ name "'Firefox Developer Edition'"
 gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ binding "'<Primary><Alt>i'"
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ command "'google-chrome-stable'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ command "'firefox-developer-edition'"
 
 # open GNOME Terminal
 gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}3/ name "'GNOME Terminal'"
