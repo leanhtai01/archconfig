0ad4b24907422e7f518839485a1d93d83ee5b917
leanhtai01 <leanhtai01@gmail.com>
2021-02-22 10:44:23 +0700

    change way to install packettracer

--- post_install/install_packettracer.sh
+++ post_install/install_packettracer.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-mkdir -p ~/.cache/yay/packettracer
+yay -Syu --noconfirm packettracer
 (cd ~/.cache/yay/packettracer && gdown https://drive.google.com/uc?id=1LBibB4uSWElbZe0gSl4tlkJ5avXCdBcO)
 yay -Syu --noconfirm packettracer
