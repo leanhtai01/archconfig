2f818716070abcf60701e83c17bd78b24972a079
leanhtai01 <leanhtai01@gmail.com>
2021-02-22 10:46:51 +0700

    ignore error when install packettracer

--- post_install/install_packettracer.sh
+++ post_install/install_packettracer.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 yay -Syu --noconfirm packettracer
 (cd ~/.cache/yay/packettracer && gdown https://drive.google.com/uc?id=1LBibB4uSWElbZe0gSl4tlkJ5avXCdBcO)
 yay -Syu --noconfirm packettracer
