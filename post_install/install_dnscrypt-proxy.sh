f532de01f99ef72f1cd2df1b4427dc5a44e93c55
leanhtai01 <leanhtai01@gmail.com>
2021-02-24 09:25:51 +0700

    using tools to enable dnscrypt-proxy

--- post_install/install_dnscrypt-proxy.sh
+++ post_install/install_dnscrypt-proxy.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
+set -e
+
+parent_dir=$(cd $(dirname $0)/..; pwd)
+
 sudo pacman -Syu --needed --noconfirm dnscrypt-proxy
-
-# back up /etc/resolv.conf file
-sudo cp /etc/resolv.conf /etc/resolv.conf.bak
-
-# modify /etc/resolv.conf
-printf "nameserver ::1\n" | sudo tee /etc/resolv.conf
-printf "nameserver 127.0.0.1\n" | sudo tee -a /etc/resolv.conf
-printf "options edns0 single-request-reopen\n" | sudo tee -a /etc/resolv.conf
-
-# prevent /etc/resolv.conf from modified
-sudo chattr +i /etc/resolv.conf
-
-# start systemd service
-sudo systemctl enable dnscrypt-proxy.service
-sudo systemctl start dnscrypt-proxy.service
-
-# restart Network Manager
-sudo systemctl restart NetworkManager
-sleep 40
+$parent_dir/tools/enable_dnscrypt-proxy.sh
+sleep 10
