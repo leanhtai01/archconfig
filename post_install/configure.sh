0c466a9a2ebfc8d71d10a2364474428c0e2b9fd4
leanhtai01 <leanhtai01@gmail.com>
2021-03-20 21:28:35 +0700

    post install: remove unused configures

--- post_install/configure.sh
+++ post_install/configure.sh
@@ -4,14 +4,9 @@ set -e
 
 current_dir=$(dirname $0)
 
-# $current_dir/install_arch_cursor_theme.sh
 $current_dir/configure_gnome.sh
 $current_dir/configure_gedit.sh
 $current_dir/configure_quadrapassel.sh
 $current_dir/make_shortcuts_gnome.sh
-# $current_dir/configure_ibus-unikey.sh
 $current_dir/configure_emacs.sh
-# $current_dir/swap_caps_left_ctrl_console.sh
 $current_dir/configure_git.sh
-$current_dir/configure_netbeans.sh
-$current_dir/configure_clamav.sh
