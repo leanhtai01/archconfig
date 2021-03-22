9bbcab18b2a0b2fe435775c9b82b44a7e740f983
leanhtai01 <leanhtai01@gmail.com>
2021-03-22 08:15:20 +0700

    not to install any games

--- system_install/install.sh
+++ system_install/install.sh
@@ -11,4 +11,3 @@ gpu=
 . $current_dir/install_gpu_driver.sh $gpu $newusername in_chroot
 . $current_dir/install_core_packages.sh $newusername in_chroot
 . $current_dir/install_desktop_environment.sh "$desktop_environment" in_chroot $newusername
-. $current_dir/install_games.sh in_chroot
