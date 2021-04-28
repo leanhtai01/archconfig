cbe2c35a3d5bbe444431e04541fb97f481d7b358
leanhtai01 <leanhtai01@gmail.com>
2021-04-28 13:03:05 +0700

    not set favorite apps

--- post_install/configure_gnome.sh
+++ post_install/configure_gnome.sh
@@ -53,7 +53,7 @@ gsettings set org.gnome.nautilus.preferences default-folder-viewer "'list-view'"
 # gsettings set org.gnome.shell favorite-apps "[]"
 
 # setup favorite-apps
-gsettings set org.gnome.shell favorite-apps "['torbrowser.desktop', 'thunderbird.desktop', 'org.keepassxc.KeePassXC.desktop', 'virt-manager.desktop', 'org.gnome.gedit.desktop']"
+# gsettings set org.gnome.shell favorite-apps "['torbrowser.desktop', 'thunderbird.desktop', 'org.keepassxc.KeePassXC.desktop', 'virt-manager.desktop', 'org.gnome.gedit.desktop']"
 
 # switch applications only in current workspace
 gsettings set org.gnome.shell.app-switcher current-workspace-only true
