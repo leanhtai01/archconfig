fee5b15b3155b11b5a7b657d3359f697884fcc2f
leanhtai01 <leanhtai01@gmail.com>
2021-04-28 21:01:12 +0700

    post install: only install packages from main repos

--- post_install/install_packages.sh
+++ post_install/install_packages.sh
@@ -4,13 +4,13 @@ set -e
 
 current_dir=$(dirname $0)
 
-$current_dir/install_yay.sh
-$current_dir/install_ibus-bamboo.sh
+# $current_dir/install_yay.sh
+# $current_dir/install_ibus-bamboo.sh
 $current_dir/configure_kvm.sh
 $current_dir/install_lamp_stack.sh
 $current_dir/configure_tlp.sh
 $current_dir/configure_ufw.sh
-$current_dir/install_sublime_tools.sh
-$current_dir/install_virtualbox-ext-oracle.sh
-$current_dir/install_gcdemu.sh
-$current_dir/install_flatpak_apps.sh
+# $current_dir/install_sublime_tools.sh
+# $current_dir/install_virtualbox-ext-oracle.sh
+# $current_dir/install_gcdemu.sh
+# $current_dir/install_flatpak_apps.sh
