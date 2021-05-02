7a94c57327dc670a5e4e3cf025b8ddb17a823030
leanhtai01 <leanhtai01@gmail.com>
2021-05-02 09:28:25 +0700

    i3 install: make tmp dir, remove tmp files after used

--- system_install/install_i3wm.sh
+++ system_install/install_i3wm.sh
@@ -16,18 +16,21 @@ $install_command i3-wm i3lock i3status dmenu rxvt-unicode xorg-xinit feh xss-loc
 
 # configure i3
 # get/create required files and dirs
-${prefix}mkdir -p /home/${newusername}/git_repos/archconfig
-${prefix}mkdir -p /home/${newusername}/git_repos/i3config
-${prefix}git clone https://github.com/leanhtai01/archconfig /home/${newusername}/git_repos/archconfig
-${prefix}git clone https://github.com/leanhtai01/i3config /home/${newusername}/git_repos/i3config
+${prefix}mkdir -p /home/${newusername}/tmp/archconfig
+${prefix}mkdir -p /home/${newusername}/tmp/i3config
+${prefix}git clone https://github.com/leanhtai01/archconfig /home/${newusername}/tmp/archconfig
+${prefix}git clone https://github.com/leanhtai01/i3config /home/${newusername}/tmp/i3config
 ${prefix}mkdir -p /home/${newusername}/.config
 
 # copy required files
-${prefix}cp -r /home/${newusername}/git_repos/i3config/i3 /home/${newusername}/.config
-${prefix}cp -r /home/${newusername}/git_repos/i3config/i3status /home/${newusername}/.config
-${prefix}cp -r /home/${newusername}/git_repos/i3config/redshift /home/${newusername}/.config
-${prefix}cp /home/${newusername}/git_repos/archconfig/data/tiling_window_manager/.Xresources /home/${newusername}
+${prefix}cp -r /home/${newusername}/tmp/i3config/i3 /home/${newusername}/.config
+${prefix}cp -r /home/${newusername}/tmp/i3config/i3status /home/${newusername}/.config
+${prefix}cp -r /home/${newusername}/tmp/i3config/redshift /home/${newusername}/.config
+${prefix}cp /home/${newusername}/tmp/archconfig/data/tiling_window_manager/.Xresources /home/${newusername}
 
 # change owner from root to user
-${prefix}chown -R ${newusername}:${newusername} /home/${newusername}/git_repos
+${prefix}chown -R ${newusername}:${newusername} /home/${newusername}/tmp
 ${prefix}chown -R ${newusername}:${newusername} /home/${newusername}/.config
+
+# remove tmp files
+${prefix}rm -rf /home/${newusername}/tmp/*
