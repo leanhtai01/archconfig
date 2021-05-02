23efbebcb66c19a120eee7aa1bcc1fc70aa30ec4
leanhtai01 <leanhtai01@gmail.com>
2021-05-02 14:19:28 +0700

    post install: install minecraft

--- post_install/install_external_packages.sh
+++ post_install/install_external_packages.sh
@@ -9,3 +9,4 @@ $current_dir/install_ibus-bamboo.sh
 $current_dir/install_virtualbox-ext-oracle.sh
 $current_dir/install_gcdemu.sh
 $current_dir/install_flatpak_apps.sh
+$current_dir/install_minecraft.sh
