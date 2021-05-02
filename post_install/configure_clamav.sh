b31d02749f5a87a219a1a99330a01cf515a43437
leanhtai01 <leanhtai01@gmail.com>
2021-05-02 15:08:56 +0700

    post install: install clamav noconfirm

--- post_install/configure_clamav.sh
+++ post_install/configure_clamav.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo pacman -Syu --needed clamav
+sudo pacman -Syu --needed --noconfirm clamav
 sudo freshclam
 sudo systemctl enable clamav-freshclam.service
 sudo systemctl enable clamav-daemon.service
