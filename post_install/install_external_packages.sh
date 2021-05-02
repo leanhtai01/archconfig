aadd6ffb69017c1eac762f49fa85f43e9462b4b1
leanhtai01 <leanhtai01@gmail.com>
2021-05-02 13:30:08 +0700

    remove installation of sublime tools

--- post_install/install_external_packages.sh
+++ post_install/install_external_packages.sh
@@ -6,7 +6,6 @@ current_dir=$(dirname $0)
 
 $current_dir/install_yay.sh
 $current_dir/install_ibus-bamboo.sh
-# $current_dir/install_sublime_tools.sh
 $current_dir/install_virtualbox-ext-oracle.sh
 $current_dir/install_gcdemu.sh
 $current_dir/install_flatpak_apps.sh
