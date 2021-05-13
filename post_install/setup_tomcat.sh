c70dda6033659dac754a336a72fc929a121333d1
leanhtai01 <leanhtai01@gmail.com>
2021-05-13 13:11:44 +0700

    install tomcat: change path to source of tomcat-users.xml

--- post_install/setup_tomcat.sh
+++ post_install/setup_tomcat.sh
@@ -3,6 +3,7 @@
 set -e
 
 version=8
+parent_dir=$(cd $(dirname $0)/..; pwd)
 
 sudo pacman -Syu --needed --noconfirm tomcat${version}
 sudo gpasswd -a leanhtai01 tomcat${version}
@@ -14,7 +15,7 @@ sudo systemctl start tomcat${version}
 mkdir ~/.tomcat${version}
 sudo cp -r /etc/tomcat${version} ~/.tomcat${version}/conf
 sudo chown -R $(id -un):$(id -gn) ~/.tomcat${version}
-cp tomcat-users.xml ~/.tomcat${version}/conf/tomcat-users.xml
+cp $parent_dir/data/tomcat-users.xml ~/.tomcat${version}/conf/tomcat-users.xml
 mkdir ~/.tomcat${version}/webapps
 ln -s /var/lib/tomcat${version}/webapps/manager ~/.tomcat${version}/webapps/manager
 mkdir ~/.tomcat${version}/temp
