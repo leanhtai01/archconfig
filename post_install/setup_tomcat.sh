c9fff7eaa1d1008efbf613a24b4b97b3ad47f239
leanhtai01 <leanhtai01@gmail.com>
2021-05-13 22:11:15 +0700

    configure tomcat users after install

--- post_install/setup_tomcat.sh
+++ post_install/setup_tomcat.sh
@@ -9,13 +9,15 @@ sudo pacman -Syu --needed --noconfirm tomcat${version}
 sudo gpasswd -a leanhtai01 tomcat${version}
 sudo systemctl enable tomcat${version}
 sudo systemctl start tomcat${version}
+sudo cp $parent_dir/data/tomcat-users.xml /usr/share/tomcat8/conf/tomcat-users.xml
+sudo chown root:tomcat${version} /usr/share/tomcat8/conf/tomcat-users.xml
 
 # for use tomcat in netbeans
 [[ -d /home/$(whoami)/.tomcat${version} ]] && rm -r /home/$(whoami)/.tomcat${version}
 mkdir /home/$(whoami)/.tomcat${version}
 sudo cp -r /etc/tomcat${version} /home/$(whoami)/.tomcat${version}/conf
 sudo chown -R $(id -un):$(id -gn) /home/$(whoami)/.tomcat${version}
-cp $parent_dir/data/tomcat-users.xml /home/$(whoami)/.tomcat${version}/conf/tomcat-users.xml
+cp $parent_dir/data/tomcat-users.xml.netbeans /home/$(whoami)/.tomcat${version}/conf/tomcat-users.xml
 mkdir /home/$(whoami)/.tomcat${version}/webapps
 ln -s /var/lib/tomcat${version}/webapps/manager /home/$(whoami)/.tomcat${version}/webapps/manager
 mkdir /home/$(whoami)/.tomcat${version}/temp
