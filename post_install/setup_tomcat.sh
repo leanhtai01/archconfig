8620223cef07381a842c10feb7129d4c8ee55be0
leanhtai01 <leanhtai01@gmail.com>
2021-05-13 13:09:22 +0700

    post install: add setup tomcat

---
+++ post_install/setup_tomcat.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+version=8
+
+sudo pacman -Syu --needed --noconfirm tomcat${version}
+sudo gpasswd -a leanhtai01 tomcat${version}
+sudo systemctl enable tomcat${version}
+sudo systemctl start tomcat${version}
+
+# for use tomcat in netbeans
+[[ -d ~/.tomcat${version} ]] && rm -r ~/.tomcat${version}
+mkdir ~/.tomcat${version}
+sudo cp -r /etc/tomcat${version} ~/.tomcat${version}/conf
+sudo chown -R $(id -un):$(id -gn) ~/.tomcat${version}
+cp tomcat-users.xml ~/.tomcat${version}/conf/tomcat-users.xml
+mkdir ~/.tomcat${version}/webapps
+ln -s /var/lib/tomcat${version}/webapps/manager ~/.tomcat${version}/webapps/manager
+mkdir ~/.tomcat${version}/temp
