a5d3cc50742f3bfdbd1286acf1897172510bbcd7
leanhtai01 <leanhtai01@gmail.com>
2021-05-14 11:20:22 +0700

    post install: setup tomcat

--- post_install/install_packages.sh
+++ post_install/install_packages.sh
@@ -6,6 +6,7 @@ current_dir=$(dirname $0)
 
 $current_dir/configure_kvm.sh
 $current_dir/install_lamp_stack.sh
+$current_dir/setup_tomcat.sh
 $current_dir/configure_tlp.sh
 $current_dir/configure_ufw.sh
 $current_dir/configure_clamav.sh
