22802ca5c7bba1582617f78154810e724b4bbb42
leanhtai01 <leanhtai01@gmail.com>
2021-05-14 21:17:40 +0700

    include servlet-api.jar in CLASSPATH

--- post_install/configure_java_CLASSPATH.sh
+++ post_install/configure_java_CLASSPATH.sh
@@ -3,4 +3,4 @@
 set -e
 
 mkdir -p /home/$(whoami)/.config/environment.d
-printf "CLASSPATH=\"\${CLASSPATH}:/usr/share/tomcat8/lib:/usr/share/java/mariadb-jdbc/mariadb-java-client.jar\"\n" > /home/$(whoami)/.config/environment.d/java_classpath.conf
+printf "CLASSPATH=\"\${CLASSPATH}:/usr/share/tomcat8/lib/servlet-api.jar:/usr/share/java/mariadb-jdbc/mariadb-java-client.jar\"\n" > /home/$(whoami)/.config/environment.d/java_classpath.conf
