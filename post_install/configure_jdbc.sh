18ac3501733aeb90593d9215e138cb527e6507e8
leanhtai01 <leanhtai01@gmail.com>
2021-05-15 15:11:49 +0700

    remove configure CLASSPATH from configure JDBC

--- post_install/configure_jdbc.sh
+++ post_install/configure_jdbc.sh
@@ -4,4 +4,3 @@ set -e
 
 yay -Syu --noconfirm mariadb-jdbc
 mkdir -p /home/$(whoami)/.config/environment.d
-printf "CLASSPATH=\"\${CLASSPATH}:/usr/share/java/mariadb-jdbc/mariadb-java-client.jar\"\n" > /home/$(whoami)/.config/environment.d/java_classpath.conf
