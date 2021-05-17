3fe25ed5bb812cabf8ab4c9429c6d14938e35425
leanhtai01 <leanhtai01@gmail.com>
2021-05-17 08:37:25 +0700

    post install: not install roslynpad

--- post_install/install_external_packages.sh
+++ post_install/install_external_packages.sh
@@ -21,7 +21,6 @@ $current_dir/install_mssql.sh
 $current_dir/install_nautilus_dropbox.sh
 $current_dir/install_packettracer.sh
 $current_dir/install_qtspim.sh
-$current_dir/install_roslynpad.sh
 $current_dir/install_skype.sh
 $current_dir/install_teamviewer.sh
 $current_dir/install_visual_studio_code.sh
