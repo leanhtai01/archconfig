25b38fad8889b36526dd9d0c7955bac1ebc7858a
leanhtai01 <leanhtai01@gmail.com>
2021-05-19 11:21:54 +0700

    i3 config: add path prefix for chroot install

--- system_install/install_i3wm.sh
+++ system_install/install_i3wm.sh
@@ -3,12 +3,14 @@
 set -e
 
 prefix=
+path_prefix=
 newusername=$2
 parent_dir=$(cd $(dirname $0)/..; pwd)
 
 if [ ! -z $1 ] && [ $1 = "in_chroot" ]
 then
     prefix="arch-chroot /mnt "
+    path_prefix="/mnt"
 fi
 
 install_command="${prefix}pacman -Syu --needed --noconfirm"
@@ -20,10 +22,10 @@ $install_command i3-wm i3lock i3status dmenu rxvt-unicode xorg-xinit feh xss-loc
 ${prefix}mkdir -p /home/${newusername}/.config
 
 # copy required files
-${prefix}cp -r $parent_dir/i3config/i3 /home/${newusername}/.config
-${prefix}cp -r $parent_dir/i3config/i3status /home/${newusername}/.config
-${prefix}cp -r $parent_dir/i3config/redshift /home/${newusername}/.config
-${prefix}cp $parent_dir/data/tiling_window_manager/.Xresources /home/${newusername}
+cp -r $parent_dir/i3config/i3 $path_prefix/home/${newusername}/.config
+cp -r $parent_dir/i3config/i3status $path_prefix/home/${newusername}/.config
+cp -r $parent_dir/i3config/redshift $path_prefix/home/${newusername}/.config
+cp $parent_dir/data/tiling_window_manager/.Xresources $path_prefix/home/${newusername}
 
 # change owner from root to user
 ${prefix}chown -R ${newusername}:${newusername} /home/${newusername}/.config
