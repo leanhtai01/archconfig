75748788ecfaba753d9265a187d0477f4b36f6e9
leanhtai01 <leanhtai01@gmail.com>
2021-05-19 14:13:56 +0700

    post install: install ibus-bamboo from AUR

--- post_install/install_ibus-bamboo.sh
+++ post_install/install_ibus-bamboo.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-mkdir -p /home/$(whoami)/my_programs/ibus-bamboo
-git clone https://github.com/BambooEngine/ibus-bamboo /home/$(whoami)/my_programs/ibus-bamboo
-(cd /home/$(whoami)/my_programs/ibus-bamboo && sudo make install)
+# mkdir -p /home/$(whoami)/my_programs/ibus-bamboo
+# git clone https://github.com/BambooEngine/ibus-bamboo /home/$(whoami)/my_programs/ibus-bamboo
+# (cd /home/$(whoami)/my_programs/ibus-bamboo && sudo make install)
+yay -Syu --noconfirm ibus-bamboo
 ibus restart
 
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
