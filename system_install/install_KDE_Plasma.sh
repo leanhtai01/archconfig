9ae40a6cc11e57bb6abf11ae6251ec41a630d5a4
leanhtai01 <leanhtai01@gmail.com>
2021-05-21 13:30:40 +0700

    remove kde-applications-meta

--- system_install/install_KDE_Plasma.sh
+++ system_install/install_KDE_Plasma.sh
@@ -11,5 +11,5 @@ fi
 
 install_command="${prefix}pacman -Syu --needed --noconfirm"
 
-$install_command plasma-meta kde-applications-meta plasma-wayland-session ark dolphin dolphin-plugins kate kleopatra konsole okular kdegraphics-thumbnailers ffmpegthumbs spectacle gwenview gnome-keyring
+$install_command plasma-meta plasma-wayland-session ark dolphin dolphin-plugins kate kleopatra konsole okular kdegraphics-thumbnailers ffmpegthumbs spectacle gwenview gnome-keyring
 # ${prefix}systemctl enable sddm
