6f15671549b76208288e3f7e07286fda516ccff7
leanhtai01 <leanhtai01@gmail.com>
2021-05-31 22:33:48 +0700

    enable bluetooth on KDE Plasma install

--- system_install/install_KDE_Plasma.sh
+++ system_install/install_KDE_Plasma.sh
@@ -13,3 +13,4 @@ install_command="${prefix}pacman -Syu --needed --noconfirm"
 
 $install_command plasma-meta plasma-wayland-session ark dolphin dolphin-plugins kate kleopatra konsole okular kdegraphics-thumbnailers ffmpegthumbs spectacle gwenview gnome-keyring
 # ${prefix}systemctl enable sddm
+# ${prefix}systemctl enable bluetooth
