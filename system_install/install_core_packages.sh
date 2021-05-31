f11ffcd375aa2c93c88916e01968a9ee6ccd1cc9
leanhtai01 <leanhtai01@gmail.com>
2021-05-31 22:25:37 +0700

    move install kdevelop and qtcreator to appropriate place

--- system_install/install_core_packages.sh
+++ system_install/install_core_packages.sh
@@ -27,14 +27,6 @@ $install_command emacs
 # programming packages
 $install_command git dia github-cli kdiff3
 
-# install kdevelop and its all optional dependencies
-$install_command kdevelop
-$install_command $(printf "$(${prefix}pacman -Qi kdevelop)" | sed -n '/^Optional Deps/,$p' | sed '/^Required By/q' | head -n -1 | cut -c19- | cut -d[ -f1 | cut -d: -f1)
-
-# install qtcreator and its all optional dependencies
-$install_command qtcreator
-$install_command $(printf "$(${prefix}pacman -Qi qtcreator)" | sed -n '/^Optional Deps/,$p' | sed '/^Required By/q' | head -n -1 | cut -c19- | cut -d[ -f1 | cut -d: -f1)
-
 # tools
 $install_command wimlib transmission-gtk keepassxc expect pacman-contrib deja-dup curl lm_sensors dosfstools ntfs-3g p7zip unarchiver gparted wget bash-completion bchunk ibus nfs-utils samba filezilla flatpak cdemu-client vhba-module-dkms cdrtools fuseiso efitools sbsigntools clamav fish rclone
 
--- system_install/install_high_performance_programming_env.sh
+++ system_install/install_high_performance_programming_env.sh
@@ -12,3 +12,11 @@ fi
 install_command="${prefix}pacman -Syu --needed --noconfirm"
 
 $install_command gdb cmake go valgrind clang gcc julia
+
+# install kdevelop and its all optional dependencies
+$install_command kdevelop
+$install_command $(printf "$(${prefix}pacman -Qi kdevelop)" | sed -n '/^Optional Deps/,$p' | sed '/^Required By/q' | head -n -1 | cut -c19- | cut -d[ -f1 | cut -d: -f1)
+
+# install qtcreator and its all optional dependencies
+$install_command qtcreator
+$install_command $(printf "$(${prefix}pacman -Qi qtcreator)" | sed -n '/^Optional Deps/,$p' | sed '/^Required By/q' | head -n -1 | cut -c19- | cut -d[ -f1 | cut -d: -f1)
