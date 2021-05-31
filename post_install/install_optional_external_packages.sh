4617b8bb7a599f7c0e14b428c9ba8af99c3880f5
leanhtai01 <leanhtai01@gmail.com>
2021-05-31 22:41:35 +0700

    add file install_optional_external_packages.sh

---
+++ post_install/install_optional_external_packages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+current_dir=$(dirname $0)
+
+$current_dir/install_azure_data_studio.sh
+$current_dir/install_dislocker.sh
+$current_dir/install_gdown.sh
+$current_dir/install_genymotion.sh
+$current_dir/install_logisim-evolution.sh
+$current_dir/install_mssql.sh
+$current_dir/install_nautilus_dropbox.sh
+$current_dir/install_packettracer.sh
+$current_dir/install_qtspim.sh
+$current_dir/install_skype.sh
+$current_dir/install_teamviewer.sh
+$current_dir/install_chrome-gnome-shell.sh
+$current_dir/install_jetbrains-toolbox.sh
