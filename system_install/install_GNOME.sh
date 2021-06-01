66f63a3f6acd5d41cee7a6ccd772c3a7bf25c4ac
leanhtai01 <leanhtai01@gmail.com>
2021-06-01 08:08:20 +0700

    make some GNOME packages as optional

--- system_install/install_GNOME.sh
+++ system_install/install_GNOME.sh
@@ -11,7 +11,14 @@ fi
 
 install_command="${prefix}pacman -Syu --needed --noconfirm"
 
-$install_command baobab eog evince file-roller gdm gnome-calculator gnome-characters gnome-clocks gnome-control-center gnome-keyring gnome-logs gnome-screenshot gnome-system-monitor gnome-terminal gnome-themes-extra nautilus sushi seahorse seahorse-nautilus chrome-gnome-shell dconf-editor gnome-tweaks gedit gedit-plugins gnome-font-viewer totem xdg-user-dirs-gtk
+# core
+$install_command baobab eog evince file-roller gdm gnome-calculator gnome-control-center gnome-keyring gnome-screenshot gnome-system-monitor gnome-terminal gnome-themes-extra nautilus sushi dconf-editor gnome-tweaks gedit gedit-plugins totem xdg-user-dirs-gtk
+
+# optional
+# $install_command gnome-characters gnome-clocks gnome-logs seahorse seahorse-nautilus gnome-font-viewer
+
+# full
 # $install_command gnome gnome-extra seahorse seahorse-nautilus gedit-plugins
+
 ${prefix}systemctl enable gdm
 ${prefix}systemctl enable bluetooth
