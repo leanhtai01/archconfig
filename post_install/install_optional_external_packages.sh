c40d35ae7fc9447f4e822e024d96dd4fd19e2123
leanhtai01 <leanhtai01@gmail.com>
2021-06-01 07:12:49 +0700

    not install genymotion

--- post_install/install_optional_external_packages.sh
+++ post_install/install_optional_external_packages.sh
@@ -7,7 +7,7 @@ current_dir=$(dirname $0)
 $current_dir/install_azure_data_studio.sh
 $current_dir/install_dislocker.sh
 $current_dir/install_gdown.sh
-$current_dir/install_genymotion.sh
+# $current_dir/install_genymotion.sh
 $current_dir/install_logisim-evolution.sh
 $current_dir/install_mssql.sh
 $current_dir/install_nautilus_dropbox.sh
