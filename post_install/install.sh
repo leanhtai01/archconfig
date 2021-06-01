30f7ce8fc58da8477a65ca9e05e3fca18a940d84
leanhtai01 <leanhtai01@gmail.com>
2021-06-01 14:15:07 +0700

    install optional external packages (commented)

--- post_install/install.sh
+++ post_install/install.sh
@@ -7,3 +7,4 @@ current_dir=$(dirname $0)
 $current_dir/configure.sh
 $current_dir/install_packages.sh
 $current_dir/install_external_packages.sh
+# $current_dir/install_optional_external_packages.sh
