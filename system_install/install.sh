033ff2efdd187dc4003f86d5b04668b87adcd796
leanhtai01 <leanhtai01@gmail.com>
2021-06-01 14:16:39 +0700

    install all packages by default

--- system_install/install.sh
+++ system_install/install.sh
@@ -23,11 +23,11 @@ fi
 . $current_dir/install_base_system.sh
 . $current_dir/install_gpu_driver.sh $gpu $newusername in_chroot
 . $current_dir/install_core_packages.sh $newusername in_chroot
-# . $current_dir/install_optional_packages.sh $newusername in_chroot
+. $current_dir/install_optional_packages.sh $newusername in_chroot
 . $current_dir/install_python_programming_env.sh in_chroot
 . $current_dir/install_java_programming_env.sh in_chroot
 . $current_dir/install_javascript_programming_env.sh in_chroot
 . $current_dir/install_high_performance_programming_env.sh in_chroot
-# . $current_dir/install_dotnet_programming_env.sh in_chroot
+. $current_dir/install_dotnet_programming_env.sh in_chroot
 . $current_dir/install_desktop_environment.sh "$desktop_environment" in_chroot $newusername
-# . $current_dir/install_games.sh in_chroot
+. $current_dir/install_games.sh in_chroot
