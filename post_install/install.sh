1ba95f2595f10a34177c1dc103dfcf5af066ac9e
leanhtai01 <leanhtai01@gmail.com>
2021-06-01 22:05:49 +0700

    post install: add option set install_type

--- post_install/install.sh
+++ post_install/install.sh
@@ -3,8 +3,14 @@
 set -e
 
 current_dir=$(dirname $0)
+install_type=core # {core full}
 
 $current_dir/configure.sh
 $current_dir/install_packages.sh
 $current_dir/install_external_packages.sh
-$current_dir/install_optional_external_packages.sh
+
+if [ $install_type = "full" ]
+then
+    $current_dir/install_optional_external_packages.sh
+fi
+
