f4fe490bcbe35fdb0388b4f41b2dbed26a4149aa
leanhtai01 <leanhtai01@gmail.com>
2021-06-01 10:23:10 +0700

    not make GNOME krusader shortcut

--- post_install/make_shortcuts_gnome.sh
+++ post_install/make_shortcuts_gnome.sh
@@ -6,34 +6,29 @@ SCHEMATOLIST="org.gnome.settings-daemon.plugins.media-keys"
 SCHEMATOITEM="org.gnome.settings-daemon.plugins.media-keys.custom-keybinding"
 PATHTOCUSTOMKEY="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom"
 
-gsettings set $SCHEMATOLIST custom-keybindings "['${PATHTOCUSTOMKEY}0/', '${PATHTOCUSTOMKEY}1/', '${PATHTOCUSTOMKEY}2/', '${PATHTOCUSTOMKEY}3/', '${PATHTOCUSTOMKEY}4/', '${PATHTOCUSTOMKEY}5/']"
+gsettings set $SCHEMATOLIST custom-keybindings "['${PATHTOCUSTOMKEY}0/', '${PATHTOCUSTOMKEY}1/', '${PATHTOCUSTOMKEY}2/', '${PATHTOCUSTOMKEY}3/', '${PATHTOCUSTOMKEY}4/']"
 
 # open Emacs
 gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}0/ name "'Emacs'"
 gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}0/ binding "'<Primary><Alt>e'"
 gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}0/ command "'emacs'"
 
-# open Krusader
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}1/ name "'Krusader'"
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}1/ binding "'<Primary><Alt>f'"
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}1/ command "'krusader'"
-
 # open Chromium
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ name "'Firefox Developer Edition'"
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ binding "'<Primary><Alt>i'"
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ command "'firefox-developer-edition'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}1/ name "'Firefox Developer Edition'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}1/ binding "'<Primary><Alt>i'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}1/ command "'firefox-developer-edition'"
 
 # open GNOME Terminal
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}3/ name "'GNOME Terminal'"
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}3/ binding "'<Primary><Alt>t'"
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}3/ command "'gnome-terminal'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ name "'GNOME Terminal'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ binding "'<Primary><Alt>t'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ command "'gnome-terminal'"
 
 # open Nautilus
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}4/ name "'Nautilus'"
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}4/ binding "'<Super>e'"
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}4/ command "'nautilus'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}3/ name "'Nautilus'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}3/ binding "'<Super>e'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}3/ command "'nautilus'"
 
 # open GVim
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}5/ name "'GVim'"
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}5/ binding "'<Primary><Alt>v'"
-gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}5/ command "'gvim'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}4/ name "'GVim'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}4/ binding "'<Primary><Alt>v'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}4/ command "'gvim'"
