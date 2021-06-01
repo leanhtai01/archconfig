d600c21fe225f1a3d51162cdd3fc7b8630411ba2
leanhtai01 <leanhtai01@gmail.com>
2021-06-01 08:17:15 +0700

    mark baobab as optional

--- system_install/install_GNOME.sh
+++ system_install/install_GNOME.sh
@@ -12,10 +12,10 @@ fi
 install_command="${prefix}pacman -Syu --needed --noconfirm"
 
 # core
-$install_command baobab eog evince file-roller gdm gnome-calculator gnome-control-center gnome-keyring gnome-screenshot gnome-system-monitor gnome-terminal gnome-themes-extra nautilus sushi gnome-tweaks gedit gedit-plugins totem xdg-user-dirs-gtk
+$install_command eog evince file-roller gdm gnome-calculator gnome-control-center gnome-keyring gnome-screenshot gnome-system-monitor gnome-terminal gnome-themes-extra nautilus sushi gnome-tweaks gedit gedit-plugins totem xdg-user-dirs-gtk
 
 # optional
-# $install_command gnome-characters gnome-clocks gnome-logs seahorse seahorse-nautilus gnome-font-viewer dconf-editor
+# $install_command gnome-characters gnome-clocks gnome-logs seahorse seahorse-nautilus gnome-font-viewer dconf-editor baobab
 
 # full
 # $install_command gnome gnome-extra seahorse seahorse-nautilus gedit-plugins
