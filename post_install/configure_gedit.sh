06c815e86b7ea5b3b4e1383d12528c7dbc8781d6
leanhtai01 <leanhtai01@gmail.com>
2021-06-08 10:02:38 +0700

    post install: not set theme gedit

--- post_install/configure_gedit.sh
+++ post_install/configure_gedit.sh
@@ -8,8 +8,8 @@ gsettings set org.gnome.gedit.preferences.editor display-right-margin true
 # insert spaces
 gsettings set org.gnome.gedit.preferences.editor insert-spaces true
 
-# set theme
-gsettings set org.gnome.gedit.preferences.editor scheme "'builder-dark'"
+# # set theme
+# gsettings set org.gnome.gedit.preferences.editor scheme "'builder-dark'"
 
 # enable plugins
 gsettings set org.gnome.gedit.plugins active-plugins "['codecomment', 'colorpicker', 'wordcompletion', 'commander', 'bracketcompletion', 'smartspaces', 'spell', 'devhelp', 'sessionsaver', 'git', 'terminal', 'sort', 'filebrowser', 'modelines', 'docinfo', 'quickhighlight', 'multiedit', 'drawspaces', 'bookmarks', 'quickopen', 'findinfiles', 'externaltools']"
