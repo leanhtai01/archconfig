462914fdfe125125d306f15934f9664b1ac67893
leanhtai01 <leanhtai01@gmail.com>
2021-06-09 13:35:43 +0700

    post install: not install gcdemu on virtual machine

--- post_install/install_external_packages.sh
+++ post_install/install_external_packages.sh
@@ -7,7 +7,6 @@ current_dir=$(dirname $0)
 $current_dir/install_yay.sh
 $current_dir/configure_java_environment.sh
 $current_dir/install_ibus-bamboo.sh
-$current_dir/install_gcdemu.sh
 $current_dir/install_flatpak_apps.sh
 $current_dir/install_sublime_tools.sh
 $current_dir/install_visual_studio_code.sh
@@ -21,4 +20,5 @@ then
     $current_dir/install_vmware_workstation.sh
     $current_dir/install_ventoy-bin.sh
     $current_dir/install_vdhcoapp.sh
+    $current_dir/install_gcdemu.sh
 fi
