3ee14c75980de97aa1922abcfb5fd27bd8ac6291
leanhtai01 <leanhtai01@gmail.com>
2021-06-14 09:51:05 +0700

    post install: set dark theme GNOME

--- post_install/configure_gnome.sh
+++ post_install/configure_gnome.sh
@@ -8,8 +8,8 @@ set -e
 # swap CapsLock - Esc
 # gsettings set org.gnome.desktop.input-sources xkb-options "['lv3:rwin_switch', 'caps:swapescape']"
 
-# # set applications theme to dark
-# gsettings set org.gnome.desktop.interface gtk-theme "'Adwaita-dark'"
+# set applications theme to dark
+gsettings set org.gnome.desktop.interface gtk-theme "'Adwaita-dark'"
 
 # set alternate characters key
 gsettings set org.gnome.desktop.input-sources xkb-options "['lv3:rwin_switch']"
