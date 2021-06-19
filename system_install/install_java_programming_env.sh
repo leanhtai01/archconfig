bfc6ee605a5353b7cf9d4f853dcaa5b3eab96113
leanhtai01 <leanhtai01@gmail.com>
2021-06-19 23:02:21 +0700

    install openjdk8

--- system_install/install_java_programming_env.sh
+++ system_install/install_java_programming_env.sh
@@ -12,3 +12,4 @@ fi
 install_command="${prefix}pacman -Syu --needed --noconfirm"
 
 $install_command jdk-openjdk openjdk-doc java-openjfx netbeans maven
+$install_command jdk8-openjdk openjdk8-doc java8-openjfx
