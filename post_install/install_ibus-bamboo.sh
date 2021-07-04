e407ec9123f6b5c29e88959f7db89ce90b9aca19
leanhtai01 <leanhtai01@gmail.com>
2021-07-04 18:00:04 +0700

    post install: restart ibus only in GNOME

--- post_install/install_ibus-bamboo.sh
+++ post_install/install_ibus-bamboo.sh
@@ -6,10 +6,10 @@ set -e
 # git clone https://github.com/BambooEngine/ibus-bamboo /home/$(whoami)/my_programs/ibus-bamboo
 # (cd /home/$(whoami)/my_programs/ibus-bamboo && sudo make install)
 yay -Syu --noconfirm ibus-bamboo
-ibus restart
 
 if [ $1 = "GNOME" ]
 then
+    ibus restart
     gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
     gsettings set org.gnome.desktop.interface gtk-im-module "'ibus'"
 fi
