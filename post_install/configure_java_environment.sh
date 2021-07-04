9768f90fa75f559393ad5624c94519b7571652a1
leanhtai01 <leanhtai01@gmail.com>
2021-07-04 21:06:49 +0700

    post install: install java packages when configure java

--- post_install/configure_java_environment.sh
+++ post_install/configure_java_environment.sh
@@ -4,6 +4,8 @@ set -e
 
 current_dir=$(dirname $0)
 
+sudo pacman -Syu --needed --noconfirm jdk-openjdk openjdk-doc java-openjfx netbeans maven
+
 $current_dir/setup_tomcat.sh
 $current_dir/configure_jdbc.sh
 $current_dir/configure_java_CLASSPATH.sh
