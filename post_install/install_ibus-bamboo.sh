af1fc2a19212f809c09e614e120ca15a1c95391d
leanhtai01 <leanhtai01@gmail.com>
2021-07-04 20:30:39 +0700

    post install: configure ibus for KDEPlasma

--- post_install/install_ibus-bamboo.sh
+++ post_install/install_ibus-bamboo.sh
@@ -2,10 +2,17 @@
 
 set -e
 
+parent_dir=$(cd $(dirname $0)/..; pwd)
+
 # mkdir -p /home/$(whoami)/my_programs/ibus-bamboo
 # git clone https://github.com/BambooEngine/ibus-bamboo /home/$(whoami)/my_programs/ibus-bamboo
 # (cd /home/$(whoami)/my_programs/ibus-bamboo && sudo make install)
 yay -Syu --noconfirm ibus-bamboo
+
+if [ $1 = "KDEPlasma" ]
+then
+    cp $parent_dir/data/ibus_config_data /home/$(whoami)/.xprofile
+fi
 
 if [ $1 = "GNOME" ]
 then
