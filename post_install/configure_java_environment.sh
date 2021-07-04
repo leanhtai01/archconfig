e8a90d37fc7003d969ba8eccb459261ce6456032
leanhtai01 <leanhtai01@gmail.com>
2021-07-04 21:24:17 +0700

    post install: configure java: not configure jdbc and java classpath

--- post_install/configure_java_environment.sh
+++ post_install/configure_java_environment.sh
@@ -7,5 +7,5 @@ current_dir=$(dirname $0)
 sudo pacman -Syu --needed --noconfirm jdk-openjdk openjdk-doc java-openjfx netbeans maven
 
 $current_dir/setup_tomcat.sh
-$current_dir/configure_jdbc.sh
-$current_dir/configure_java_CLASSPATH.sh
+# $current_dir/configure_jdbc.sh
+# $current_dir/configure_java_CLASSPATH.sh
