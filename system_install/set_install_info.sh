921bc9d8e88ba75d9fcf4ae01d14cc4dda3fa7df
leanhtai01 <leanhtai01@gmail.com>
2021-07-12 07:42:29 +0700

    system install: move set info to another file

---
+++ system_install/set_install_info.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+install_dev= # nvme0n1
+size_of_ram=
+newusername="leanhtai01"
+realname="Lê Anh Tài"
+user_choice= # 1 - normal install, 2 - LVM on LUKS, 4 - dual-boot with Windows 10 (normal install), 5- dual-boot with Windows 10 (LVM on LUKS)
+bootloader= # 1 - systemd-boot, 2 - GRUB (encrypted boot), 3 - GRUB (non-encrypted boot)
+setupsecureboot=
+setupkeytool=
+desktop_environment= # {GNOME KDEPlasma i3 none}
+desktop_install_type=core # {core full}
+gpu= # {intel, amd, nvidia, virtualbox, vmware}
+hostname=
+system_install_type="core" # {core optional python java javascript high_performance dotnet desktop games}
