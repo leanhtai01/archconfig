87c17b7ec1f6b3a754b1ab957b83759a1a4e4425
leanhtai01 <leanhtai01@gmail.com>
2021-07-13 16:16:20 +0700

    system install: move high performance programming packages to core packages

--- system_install/install_core_packages.sh
+++ system_install/install_core_packages.sh
@@ -26,6 +26,15 @@ $install_command emacs
 
 # programming packages
 $install_command git dia github-cli kdiff3
+$install_command gdb cmake go valgrind clang gcc
+
+# install kdevelop and its all optional dependencies
+$install_command kdevelop
+$install_command $(printf "$(${prefix}pacman -Qi kdevelop)" | sed -n '/^Optional Deps/,$p' | sed '/^Required By/q' | head -n -1 | cut -c19- | cut -d[ -f1 | cut -d: -f1)
+
+# install qtcreator and its all optional dependencies
+$install_command qtcreator
+$install_command $(printf "$(${prefix}pacman -Qi qtcreator)" | sed -n '/^Optional Deps/,$p' | sed '/^Required By/q' | head -n -1 | cut -c19- | cut -d[ -f1 | cut -d: -f1)
 
 # tools
 $install_command keepassxc expect pacman-contrib curl dosfstools p7zip unarchiver bash-completion ibus flatpak tree
