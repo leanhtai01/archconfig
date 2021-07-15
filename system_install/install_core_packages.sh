69bad0ea6f301cc2afee8b0fd9e489b7c3ddc48e
leanhtai01 <leanhtai01@gmail.com>
2021-07-15 14:16:14 +0700

    system install: add npm to core packages

--- system_install/install_core_packages.sh
+++ system_install/install_core_packages.sh
@@ -19,7 +19,7 @@ $install_command ttf-dejavu ttf-liberation noto-fonts-emoji ttf-hack adobe-sourc
 $install_command pulseaudio-alsa alsa-utils
 
 # browsers
-$install_command chromium
+$install_command chromium npm
 
 # editors
 $install_command emacs
