954b551088acaa635b5413ceec867638e330017c
leanhtai01 <leanhtai01@gmail.com>
2021-08-06 13:09:39 +0700

    system install: add setup connect android phone to core packages

--- system_install/install_core_packages.sh
+++ system_install/install_core_packages.sh
@@ -39,6 +39,9 @@ $install_command $(printf "$(${prefix}pacman -Qi qtcreator)" | sed -n '/^Optiona
 # tools
 $install_command keepassxc expect pacman-contrib curl dosfstools p7zip unarchiver bash-completion flatpak tree fish ibus
 
+# setup connect android phone
+$install_command mtpfs gvfs-mtp gvfs-gphoto2
+
 # install krusader and its all optional dependencies
 $install_command krusader
 $install_command $(printf "$(${prefix}pacman -Qi krusader)" | sed -n '/^Optional Deps/,$p' | sed '/^Required By/q' | head -n -1 | cut -c19- | cut -d[ -f1 | cut -d: -f1)
