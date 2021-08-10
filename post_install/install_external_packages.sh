0bbb4f342eccde65eb02f7cebe8e0af896d62024
leanhtai01 <leanhtai01@gmail.com>
2021-08-10 13:34:06 +0700

    post install: re-install some removed packages

--- post_install/install_external_packages.sh
+++ post_install/install_external_packages.sh
@@ -14,8 +14,10 @@ fi
 $current_dir/install_ibus-bamboo.sh $2
 $current_dir/install_flatpak_apps.sh
 $current_dir/install_google-chrome.sh
+$current_dir/install_chrome-gnome-shell.sh
 
 if [ $1 != "virtualbox" ]
 then
+    $current_dir/install_cdemu.sh
     $current_dir/install_ventoy-bin.sh
 fi
