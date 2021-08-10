3107e3c6fa0d097976e9835a7d36bc04bda5e6f1
leanhtai01 <leanhtai01@gmail.com>
2021-08-10 13:48:15 +0700

    post install: configure GNOME: set nautilus initial size

--- post_install/configure_gnome.sh
+++ post_install/configure_gnome.sh
@@ -69,7 +69,7 @@ gsettings set org.gnome.software download-updates false
 gsettings set org.gnome.software download-updates-notify false
 
 # set nautilus initial-size
-# gsettings set org.gnome.nautilus.window-state initial-size "(1169, 785)"
+gsettings set org.gnome.nautilus.window-state initial-size "(1169, 785)"
 
 # sort directories first
 # gsettings set org.gtk.settings.file-chooser sort-directories-first true
