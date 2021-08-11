3a39f14e9fc4a8bfcf20d850699c0f987e606497
leanhtai01 <leanhtai01@gmail.com>
2021-08-11 16:13:00 +0700

    post install: install sublime tools

--- post_install/install_external_packages.sh
+++ post_install/install_external_packages.sh
@@ -12,6 +12,7 @@ then
 fi
 
 $current_dir/install_ibus-bamboo.sh $2
+$current_dir/install_sublime_tools.sh
 $current_dir/install_visual_studio_code.sh
 $current_dir/install_flatpak_apps.sh
 $current_dir/install_google-chrome.sh
