1e4309cd5720ce876b7f82a7af9de86a8ea38892
leanhtai01 <leanhtai01@gmail.com>
2021-08-14 08:06:12 +0700

    post install: change packettracer download repos

--- post_install/install_packettracer.sh
+++ post_install/install_packettracer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 yay -Syu --noconfirm packettracer
-(cd ~/.cache/yay/packettracer && gdown https://drive.google.com/uc?id=1LBibB4uSWElbZe0gSl4tlkJ5avXCdBcO)
+(cd ~/.cache/yay/packettracer && gdown https://drive.google.com/uc?id=182jOUFoixt9KM3M4mnO7WO9gNfss_oQa)
 yay -Syu --noconfirm packettracer
