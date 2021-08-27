d8c98068da6d84040fdc35272c182d7adc84fd51
leanhtai01 <leanhtai01@gmail.com>
2021-08-28 05:58:24 +0700

    post install: move skype and jetbrains-toolbox to optional

--- post_install/install_optional_external_packages.sh
+++ post_install/install_optional_external_packages.sh
@@ -11,3 +11,5 @@ $current_dir/install_qtspim.sh
 $current_dir/install_teamviewer.sh
 $current_dir/install_vdhcoapp.sh
 $current_dir/install_zoom.sh
+$current_dir/install_skype.sh
+$current_dir/install_jetbrains-toolbox.sh
