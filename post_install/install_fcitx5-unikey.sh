67ecddc37aef83e07317c5c1aaa2b570d3fd8cc8
leanhtai01 <leanhtai01@gmail.com>
2021-08-30 08:59:49 +0700

    post install: add packages for fcitx5

--- post_install/install_fcitx5-unikey.sh
+++ post_install/install_fcitx5-unikey.sh
@@ -4,7 +4,7 @@ set -e
 
 parent_dir=$(cd $(dirname $0)/..; pwd)
 
-sudo pacman -Syu --needed --noconfirm fcitx5-im fcitx5-unikey
+sudo pacman -Syu --needed --noconfirm fcitx5-im fcitx5-unikey fcitx5-qt fcitx5-gtk
 
 mkdir -p /home/$(whoami)/.config/environment.d
 cp $parent_dir/data/fcitx5.conf /home/$(whoami)/.config/environment.d/
