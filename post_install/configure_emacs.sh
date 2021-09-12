6916b03d193831c5ac23478437c9955a6e3d7c80
leanhtai01 <leanhtai01@gmail.com>
2021-09-12 20:50:39 +0700

    configure emacs: shorten copy command

--- post_install/configure_emacs.sh
+++ post_install/configure_emacs.sh
@@ -6,5 +6,5 @@ parent_dir=$(cd $(dirname $0)/..; pwd)
 
 mkdir -p /home/$(whoami)/backup_files_emacs
 mkdir -p /home/$(whoami)/desktop_saves_emacs
-cp -r $parent_dir/data/emacs_backup/.emacs $parent_dir/data/emacs_backup/.emacs.d /home/$(whoami)/
-sudo cp -r $parent_dir/data/emacs_backup/.emacs $parent_dir/data/emacs_backup/.emacs.d /root/
+cp -r $parent_dir/data/emacs_backup/{.emacs,.emacs.d} /home/$(whoami)/
+sudo cp -r $parent_dir/data/emacs_backup/{.emacs,.emacs.d} /root/
