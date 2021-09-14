b97712401bd619b9878bbfdc2e9ac5dbab785e6f
leanhtai01 <leanhtai01@gmail.com>
2021-09-14 17:48:20 +0700

    system install: experiment: install tlp

---
+++ experiment/configure_tlp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+prefix=
+
+if [ ! -z $1 ] && [ $1 = "in_chroot" ]
+then
+    prefix="arch-chroot /mnt "
+fi
+
+install_command="${prefix}pacman -Syu --needed --noconfirm"
+
+$install_command tlp acpi_call
+$install_command systemctl enable tlp
+$install_command systemctl start tlp
