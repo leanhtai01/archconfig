44105a96b4d913dc751f4e5866caefe976d09735
leanhtai01 <leanhtai01@gmail.com>
2021-09-15 07:08:04 +0700

    experiment: configure kvm

---
+++ experiment/configure_kvm.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+prefix=
+
+if [ ! -z $1 ] && [ $1 = "in_chroot" ]
+then
+    prefix="arch-chroot /mnt "
+fi
+
+install_command="${prefix}pacman -Syu --needed --noconfirm"
+
+INSTALL_KVM=$($prefix expect -c "
+spawn pacman -Syu --needed virt-manager qemu vde2 dnsmasq bridge-utils virt-viewer dmidecode edk2-ovmf cockpit cockpit-machines iptables-nft
+
+expect \":: iptables-nft and iptables are in conflict. Remove iptables? \[y/N\]\"
+send \"y\r\"
+
+expect eof
+
+expect \":: Proceed with installation? \[Y/n\]\"
+send \"y\r\"
+
+expect eof
+")
+
+echo "${INSTALL_KVM}"
+
+$prefix systemctl enable libvirtd
+$prefix systemctl start libvirtd
+$prefix systemctl enable cockpit.socket
+$prefix systemctl start cockpit.socket
+
+$prefix sed -i "/^#unix_sock_group = \"libvirt\"$/s/^#//" /etc/libvirt/libvirtd.conf
+$prefix sed -i "/^#unix_sock_rw_perms = \"0770\"$/s/^#//" /etc/libvirt/libvirtd.conf
+$prefix gpasswd -a $2 libvirt
+$prefix gpasswd -a $2 kvm
