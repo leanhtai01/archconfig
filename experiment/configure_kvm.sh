f2869e6aa46007874025934a0dc48ad204987691
leanhtai01 <leanhtai01@gmail.com>
2021-09-19 12:59:16 +0700

    experiment: configure kvm: using single quotes outside

--- experiment/configure_kvm.sh
+++ experiment/configure_kvm.sh
@@ -14,21 +14,21 @@ fi
 # update system before install
 $prefix pacman -Syu --needed --noconfirm
 
-INSTALL_KVM=$($prefix expect -c "
+INSTALL_KVM=$($prefix expect -c '
 spawn pacman -Syu --needed virt-manager qemu vde2 dnsmasq bridge-utils virt-viewer dmidecode edk2-ovmf cockpit cockpit-machines iptables-nft
 
-expect \":: iptables-nft and iptables are in conflict. Remove iptables? \[y/N\] \"
-send \"y\r\"
+expect ":: iptables-nft and iptables are in conflict. Remove iptables? \[y/N\] "
+send "y\r"
 
-expect \":: Proceed with installation? \[Y/n\] \"
-send \"y\r\"
+expect ":: Proceed with installation? \[Y/n\] "
+send "y\r"
 
-expect -re \"^Adding user.*to group kvm\"
+expect -re "^Adding user.*to group kvm"
 
 expect eof
 
 exit 0
-")
+')
 
 echo "${INSTALL_KVM}"
 
