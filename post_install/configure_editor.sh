78f4c567bdf7e3c280c7bc33713e88bba1b7a922
leanhtai01 <leanhtai01@gmail.com>
2021-09-26 17:18:30 +0700

    make necessary directory for emacs

--- post_install/configure_editor.sh
+++ post_install/configure_editor.sh
@@ -6,6 +6,8 @@ username=$1
 
 function getconfig() {
     cd /home/${username}
+    mkdir -p backup_files_emacs
+    mkdir -p desktop_saves_emacs
     git init -b main
     git remote add origin https://github.com/leanhtai01/editorconfig
     git pull --ff-only --set-upstream origin main
