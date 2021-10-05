6d9a7c8a5eb2aa1e77d5e42b639d27f9322e9664
leanhtai01 <leanhtai01@gmail.com>
2021-10-05 16:16:22 +0700

    tools: toggle touchpad: toggle only tap-to-click

--- tools/toggle_touchpad_gnome.sh
+++ tools/toggle_touchpad_gnome.sh
@@ -3,23 +3,23 @@
 set -e
 
 # get the current values
-two_finger_scrolling_enabled=$(dconf read /org/gnome/desktop/peripherals/touchpad/two-finger-scrolling-enabled)
-edge_scrolling_enabled=$(dconf read /org/gnome/desktop/peripherals/touchpad/edge-scrolling-enabled)
+# two_finger_scrolling_enabled=$(dconf read /org/gnome/desktop/peripherals/touchpad/two-finger-scrolling-enabled)
+# edge_scrolling_enabled=$(dconf read /org/gnome/desktop/peripherals/touchpad/edge-scrolling-enabled)
 tap_to_click=$(dconf read /org/gnome/desktop/peripherals/touchpad/tap-to-click)
 
-if [ $two_finger_scrolling_enabled = "true" ]
-then
-    gsettings set org.gnome.desktop.peripherals.touchpad two-finger-scrolling-enabled false
-else
-    gsettings set org.gnome.desktop.peripherals.touchpad two-finger-scrolling-enabled true
-fi
+# if [ $two_finger_scrolling_enabled = "true" ]
+# then
+#     gsettings set org.gnome.desktop.peripherals.touchpad two-finger-scrolling-enabled false
+# else
+#     gsettings set org.gnome.desktop.peripherals.touchpad two-finger-scrolling-enabled true
+# fi
 
-if [ $edge_scrolling_enabled = "true" ]
-then
-    gsettings set org.gnome.desktop.peripherals.touchpad edge-scrolling-enabled false
-else
-    gsettings set org.gnome.desktop.peripherals.touchpad edge-scrolling-enabled true
-fi
+# if [ $edge_scrolling_enabled = "true" ]
+# then
+#     gsettings set org.gnome.desktop.peripherals.touchpad edge-scrolling-enabled false
+# else
+#     gsettings set org.gnome.desktop.peripherals.touchpad edge-scrolling-enabled true
+# fi
 
 if [ $tap_to_click = "true" ]
 then
