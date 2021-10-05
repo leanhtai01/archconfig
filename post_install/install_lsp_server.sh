f35f8167190cd217c1a1eebfa09db977699001df
leanhtai01 <leanhtai01@gmail.com>
2021-10-05 10:01:09 +0700

    install lsp server: install dependencies first

--- post_install/install_lsp_server.sh
+++ post_install/install_lsp_server.sh
@@ -2,6 +2,18 @@
 
 set -e
 
+if [ ! -z $1 ] && [ $1 = "in_chroot" ]
+then
+    prefix="arch-chroot /mnt "
+else
+    prefix="sudo "
+fi
+
+install_command="${prefix}pacman -Syu --needed --noconfirm"
+
+# install dependencies
+$install_command npm
+
 sudo npm i intelephense -g
 sudo npm i -g bash-language-server
 sudo npm install -g vscode-css-languageserver-bin
