1b41bcf5f34e8df429d0b4ae4e84514ae15c98fd
leanhtai01 <leanhtai01@gmail.com>
2021-10-06 09:18:48 +0700

    post install: flatpak apps: add Albion Online

--- post_install/install_flatpak_apps.sh
+++ post_install/install_flatpak_apps.sh
@@ -7,3 +7,4 @@ flatpak update
 flatpak install com.belmoussaoui.Authenticator -y
 flatpak install com.getferdi.Ferdi -y
 flatpak install io.github.hakuneko.HakuNeko -y
+flatpak install com.albiononline.AlbionOnline -y
