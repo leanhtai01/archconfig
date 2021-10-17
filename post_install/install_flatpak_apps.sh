a7c0f61b69105611f3e9e775deb8e398245de55b
leanhtai01 <leanhtai01@gmail.com>
2021-10-17 08:38:36 +0700

    post install: install flightgear

--- post_install/install_flatpak_apps.sh
+++ post_install/install_flatpak_apps.sh
@@ -8,3 +8,4 @@ flatpak install com.belmoussaoui.Authenticator -y
 flatpak install com.getferdi.Ferdi -y
 flatpak install io.github.hakuneko.HakuNeko -y
 flatpak install com.albiononline.AlbionOnline -y
+flatpak install org.flightgear.FlightGear -y
