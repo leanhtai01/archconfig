38fa267a62a7e9be7b6bfb871e3f88ae86c5a50d
leanhtai01 <leanhtai01@gmail.com>
2021-10-20 16:15:06 +0700

    post install: configure pipewire

--- post_install/configure.sh
+++ post_install/configure.sh
@@ -19,6 +19,7 @@ then
     $current_dir/configure_xsettingsd.sh
 fi
 
+$current_dir/configure_pipewire.sh
 $current_dir/configure_editor.sh $(whoami)
 $current_dir/configure_git.sh
 $current_dir/configure_clamav.sh
