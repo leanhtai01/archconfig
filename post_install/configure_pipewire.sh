019930fffe3cfa3b1fccc07a0e02133728e8f355
leanhtai01 <leanhtai01@gmail.com>
2021-10-20 16:30:02 +0700

    post install: configure pipewire: copy all config to user space

--- post_install/configure_pipewire.sh
+++ post_install/configure_pipewire.sh
@@ -3,6 +3,6 @@
 set -e
 
 mkdir -p ~/.config/pipewire
-cp /usr/share/pipewire/{client.conf,pipewire-pulse.conf} ~/.config/pipewire
+cp -r /usr/share/pipewire/* ~/.config/pipewire
 sed -i '/resample.quality/s/#//; /resample.quality/s/4/10/' ~/.config/pipewire/{client.conf,pipewire-pulse.conf}
 systemctl --user enable pipewire-media-session.service
