df9bddb2fbaf2ebb594560b4e54833b78bdea227
leanhtai01 <leanhtai01@gmail.com>
2021-10-23 22:50:02 +0700

    post install: configure zsh: using autosuggestions

--- post_install/configure_zsh.sh
+++ post_install/configure_zsh.sh
@@ -14,3 +14,4 @@ install_command="${prefix}pacman -Syu --needed --noconfirm"
 $install_command zsh zsh-completions grml-zsh-config zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting
 printf "source /etc/zsh/zshrc\n" > ~/.zshrc
 printf "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh\n" >> ~/.zshrc
+printf "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
