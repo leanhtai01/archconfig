fba381b2d5d546a9ad99e122ad8d4138aece5fa1
leanhtai01 <leanhtai01@gmail.com>
2021-10-23 22:28:00 +0700

    post install: set gnome theme: not set icons and cursor theme

--- post_install/set_gnome_themes.sh
+++ post_install/set_gnome_themes.sh
@@ -7,8 +7,8 @@ set -e
 
 # set Nordic theme
 gsettings set org.gnome.desktop.interface gtk-theme "'Nordic-darker-v40'"
-gsettings set org.gnome.desktop.interface icon-theme "'Nordic-Darker'"
+# gsettings set org.gnome.desktop.interface icon-theme "'Nordic-Darker'"
 # gsettings set org.gnome.shell.extensions.user-theme name "'Nordic-darker-v40'"
 
-# set cursor theme
-gsettings set org.gnome.desktop.interface cursor-theme "'ArchCursorTheme'"
+# # set cursor theme
+# gsettings set org.gnome.desktop.interface cursor-theme "'ArchCursorTheme'"
