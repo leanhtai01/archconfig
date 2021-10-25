eff4c469242f4ee0a1838c4c032a5610bc295879
leanhtai01 <leanhtai01@gmail.com>
2021-10-25 22:15:13 +0700

    system install: install GNOME: remove package gnome-software, gnome-software-packagekit-plugin

--- system_install/install_GNOME.sh
+++ system_install/install_GNOME.sh
@@ -15,7 +15,7 @@ install_command="${prefix}pacman -Syu --needed --noconfirm"
 if [ $desktop_install_type = "core" ]
 then
     # core
-    $install_command baobab eog evince file-roller gdm gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-contacts gnome-control-center gnome-font-viewer gnome-keyring gnome-logs gnome-screenshot gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra gnome-user-docs gnome-user-share gnome-video-effects nautilus sushi gnome-tweaks gedit gedit-plugins totem xdg-user-dirs-gtk seahorse seahorse-nautilus devhelp ghex glade gnome-builder gnome-code-assistance gnome-devel-docs gnome-usage gnome-todo gnome-software gnome-software-packagekit-plugin power-profiles-daemon
+    $install_command baobab eog evince file-roller gdm gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-contacts gnome-control-center gnome-font-viewer gnome-keyring gnome-logs gnome-screenshot gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra gnome-user-docs gnome-user-share gnome-video-effects nautilus sushi gnome-tweaks gedit gedit-plugins totem xdg-user-dirs-gtk seahorse seahorse-nautilus devhelp ghex glade gnome-builder gnome-code-assistance gnome-devel-docs gnome-usage gnome-todo power-profiles-daemon
 else
     # full
     $install_command gnome gnome-extra gedit-plugins
