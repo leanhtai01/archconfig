2ae7a3a59c1f9b5eaeff37a0382b9d601463f0f6
leanhtai01 <leanhtai01@gmail.com>
2021-10-25 17:39:08 +0700

    post install: install external packages: not install bunch of un-needed packages

--- post_install/install_external_packages.sh
+++ post_install/install_external_packages.sh
@@ -13,14 +13,14 @@ fi
 
 $current_dir/install_virtualbox-ext-oracle.sh
 $current_dir/install_ibus-bamboo.sh $2
-$current_dir/install_visual_studio_code.sh
+# $current_dir/install_visual_studio_code.sh
 $current_dir/install_flatpak_apps.sh
 $current_dir/install_google-chrome.sh
 $current_dir/install_chrome-gnome-shell.sh
-$current_dir/install_gdown.sh
-$current_dir/install_packettracer.sh
+# $current_dir/install_gdown.sh
+# $current_dir/install_packettracer.sh
 $current_dir/install_nautilus_dropbox.sh
-$current_dir/install_minecraft.sh
+# $current_dir/install_minecraft.sh
 
 if [ $1 != "virtualbox" ]
 then
@@ -28,8 +28,8 @@ then
     $current_dir/install_ventoy-bin.sh
 fi
 
-$current_dir/install_sublime_tools.sh
-$current_dir/install_dislocker.sh
-$current_dir/install_mssql.sh
-$current_dir/install_azure_data_studio.sh
+# $current_dir/install_sublime_tools.sh
+# $current_dir/install_dislocker.sh
+# $current_dir/install_mssql.sh
+# $current_dir/install_azure_data_studio.sh
 $current_dir/install_heroku.sh
