7ebe91b7b73f3cb28eeba7d7cc12fabc42aa4936
leanhtai01 <leanhtai01@gmail.com>
2021-10-25 22:42:58 +0700

    post install: configure: set GNOME theme: set Adwaita Dark theme

--- post_install/set_gnome_themes.sh
+++ post_install/set_gnome_themes.sh
@@ -3,10 +3,10 @@
 set -e
 
 # set Adwaita Dark theme
-# gsettings set org.gnome.desktop.interface gtk-theme "'Adwaita-dark'"
+gsettings set org.gnome.desktop.interface gtk-theme "'Adwaita-dark'"
 
 # set Nordic theme
-gsettings set org.gnome.desktop.interface gtk-theme "'Nordic-darker-v40'"
+# gsettings set org.gnome.desktop.interface gtk-theme "'Nordic-darker-v40'"
 # gsettings set org.gnome.desktop.interface icon-theme "'Nordic-Darker'"
 # gsettings set org.gnome.shell.extensions.user-theme name "'Nordic-darker-v40'"
 
