df17b597646381d40b031fd72926f0da6a155c46
leanhtai01 <leanhtai01@gmail.com>
2021-10-25 23:09:50 +0700

    system install: install GNOME: remove packages gnome-user-docs, gnome-user-share

--- system_install/install_GNOME.sh
+++ system_install/install_GNOME.sh
@@ -15,7 +15,7 @@ install_command="${prefix}pacman -Syu --needed --noconfirm"
 if [ $desktop_install_type = "core" ]
 then
     # core
-    $install_command baobab eog evince file-roller gdm gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-control-center gnome-font-viewer gnome-keyring gnome-logs gnome-screenshot gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra gnome-user-docs gnome-user-share gnome-video-effects nautilus sushi gnome-tweaks totem xdg-user-dirs-gtk seahorse seahorse-nautilus gnome-usage gnome-todo power-profiles-daemon
+    $install_command baobab eog evince file-roller gdm gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-control-center gnome-font-viewer gnome-keyring gnome-logs gnome-screenshot gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra gnome-video-effects nautilus sushi gnome-tweaks totem xdg-user-dirs-gtk seahorse seahorse-nautilus gnome-usage gnome-todo power-profiles-daemon
 else
     # full
     $install_command gnome gnome-extra gedit-plugins
