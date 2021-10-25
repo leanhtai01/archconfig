11e1f8d1b4b0fc701f706c727f5a84c83d487b87
leanhtai01 <leanhtai01@gmail.com>
2021-10-25 22:58:56 +0700

    post install: install flatpak apps: not install ferdi

--- post_install/install_flatpak_apps.sh
+++ post_install/install_flatpak_apps.sh
@@ -5,7 +5,7 @@ set -e
 sudo pacman -Syu --noconfirm
 flatpak update
 flatpak install com.belmoussaoui.Authenticator -y
-flatpak install com.getferdi.Ferdi -y
+# flatpak install com.getferdi.Ferdi -y
 flatpak install io.github.hakuneko.HakuNeko -y
 # flatpak install com.albiononline.AlbionOnline -y
 # flatpak install org.flightgear.FlightGear -y
