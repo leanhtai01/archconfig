be86dafa3a685d057e2d21d4a90c5d34f1720138
leanhtai01 <leanhtai01@gmail.com>
2021-10-25 22:27:15 +0700

    system install: install GNOME: move GNOME dev packages to separate script

---
+++ system_install/install_GNOME_programming.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+prefix=
+
+if [ ! -z $1 ] && [ $1 = "in_chroot" ]
+then
+    prefix="arch-chroot /mnt "
+fi
+
+install_command="${prefix}pacman -Syu --needed --noconfirm"
+
+$install_command devhelp glade gnome-builder gnome-code-assistance gnome-devel-docs
