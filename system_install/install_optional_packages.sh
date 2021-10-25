061f20efb0213f34aff3bd860eb17ca1aeaee72c
leanhtai01 <leanhtai01@gmail.com>
2021-10-25 22:10:55 +0700

    system install: move kdevelop, qtcreator from core to optional

--- system_install/install_optional_packages.sh
+++ system_install/install_optional_packages.sh
@@ -14,6 +14,14 @@ install_command="${prefix}pacman -Syu --needed --noconfirm"
 # editor
 $install_command gvim
 
+# install kdevelop and its all optional dependencies
+$install_command kdevelop
+$install_command $(printf "$(${prefix}pacman -Qi kdevelop)" | sed -n '/^Optional Deps/,$p' | sed '/^Required By/q' | head -n -1 | cut -c19- | cut -d[ -f1 | cut -d: -f1)
+
+# install qtcreator and its all optional dependencies
+$install_command qtcreator
+$install_command $(printf "$(${prefix}pacman -Qi qtcreator)" | sed -n '/^Optional Deps/,$p' | sed '/^Required By/q' | head -n -1 | cut -c19- | cut -d[ -f1 | cut -d: -f1)
+
 # tools
 $install_command gprename pdftk bleachbit aircrack-ng reflector youtube-dl nfs-utils samba wget rclone
 
