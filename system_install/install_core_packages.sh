72379d9d085dd4d8e3566cabc2222b72fe2621fc
leanhtai01 <leanhtai01@gmail.com>
2021-10-25 21:44:31 +0700

    system install: move gimp, inkscape from optional to core

--- system_install/install_core_packages.sh
+++ system_install/install_core_packages.sh
@@ -68,7 +68,7 @@ then
     $install_command calibre kchmviewer kolourpaint goldendict okular foliate
 
     # multimedia
-    $install_command kdenlive frei0r-plugins handbrake handbrake-cli
+    $install_command kdenlive frei0r-plugins handbrake handbrake-cli inkscape gimp
 
     # virtualbox
     $install_command virtualbox virtualbox-guest-iso virtualbox-host-dkms
