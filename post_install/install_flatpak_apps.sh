4435c72e56198845b8236d4abe44d445bd075c05
leanhtai01 <leanhtai01@gmail.com>
2021-10-26 19:29:48 +0700

    post install: install hakuneko from AUR

--- post_install/install_flatpak_apps.sh
+++ post_install/install_flatpak_apps.sh
@@ -6,6 +6,6 @@ sudo pacman -Syu --noconfirm
 flatpak update
 flatpak install com.belmoussaoui.Authenticator -y
 # flatpak install com.getferdi.Ferdi -y
-flatpak install io.github.hakuneko.HakuNeko -y
+# flatpak install io.github.hakuneko.HakuNeko -y
 # flatpak install com.albiononline.AlbionOnline -y
 # flatpak install org.flightgear.FlightGear -y
