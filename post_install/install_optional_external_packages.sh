90462c645f6bd3a0af45ee75d5cdad78ddb58a1a
leanhtai01 <leanhtai01@gmail.com>
2021-10-31 15:32:29 +0700

    post install: install zoom from flatpak

--- post_install/install_optional_external_packages.sh
+++ post_install/install_optional_external_packages.sh
@@ -6,7 +6,7 @@ current_dir=$(dirname $0)
 
 $current_dir/install_birdtray.sh
 # $current_dir/install_vdhcoapp.sh
-$current_dir/install_zoom.sh
+# $current_dir/install_zoom.sh
 # $current_dir/install_skype.sh
 # $current_dir/install_jetbrains-toolbox.sh
 # $current_dir/install_logisim-evolution.sh
