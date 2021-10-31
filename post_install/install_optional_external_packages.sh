850af625af717dc95633925d0f159d2da04fa8f8
leanhtai01 <leanhtai01@gmail.com>
2021-10-31 12:16:52 +0700

    post install: not install birdtray

--- post_install/install_optional_external_packages.sh
+++ post_install/install_optional_external_packages.sh
@@ -4,7 +4,7 @@ set -e
 
 current_dir=$(dirname $0)
 
-$current_dir/install_birdtray.sh
+# $current_dir/install_birdtray.sh
 # $current_dir/install_vdhcoapp.sh
 $current_dir/install_zoom.sh
 # $current_dir/install_skype.sh
