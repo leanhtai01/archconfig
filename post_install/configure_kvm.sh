b4c3383a12feef6fe2072c8ee8fbfd8978a921d0
leanhtai01 <leanhtai01@gmail.com>
2021-10-31 12:56:53 +0700

    post install: not install cockpit

--- post_install/configure_kvm.sh
+++ post_install/configure_kvm.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-sudo pacman -Syu --needed --noconfirm virt-manager qemu vde2 dnsmasq bridge-utils virt-viewer dmidecode edk2-ovmf cockpit cockpit-machines iptables-nft
+sudo pacman -Syu --needed --noconfirm virt-manager qemu vde2 dnsmasq bridge-utils virt-viewer dmidecode edk2-ovmf iptables-nft
 sudo systemctl enable libvirtd
 sudo systemctl start libvirtd
-sudo systemctl enable cockpit.socket
-sudo systemctl start cockpit.socket
+# sudo systemctl enable cockpit.socket
+# sudo systemctl start cockpit.socket
 
 sudo sed -i "/^#unix_sock_group = \"libvirt\"$/s/^#//" /etc/libvirt/libvirtd.conf
 sudo sed -i "/^#unix_sock_rw_perms = \"0770\"$/s/^#//" /etc/libvirt/libvirtd.conf
