7aa04d8aa9c9e9d3958da790fd504e32833c6b2d
leanhtai01 <leanhtai01@gmail.com>
2021-10-31 16:43:00 +0700

    post install: add script configure auto mount encrypted disk

---
+++ post_install/configure_auto_mount_encrypt_disks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+password=
+
+# ask user for disk's password
+if [ -z "$password" ]
+then
+    read -e -p "Enter disk's password: " password
+fi
+
+# create keyfile
+sudo mkdir -p /etc/luks-keys/
+printf "$password" | tee /etc/luks-keys/luks_data_drive
+
+printf 'luks_data_drive UUID=4d9848e9-a41c-41b9-aadf-b514abd02ecc /etc/luks-keys/luks_data_drive nofail\n' | sudo tee -a /etc/crypttab > /dev/null
+printf '/dev/disk/by-uuid/5650d5fa-aec0-4941-889f-6b785f016ca6 /run/media/leanhtai01/data_drive auto nosuid,nodev,nofail,x-gvfs-show 0 0\n' | sudo tee -a /etc/fstab > /dev/null
