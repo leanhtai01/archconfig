f0753574cc5ed17cf6098858d7a27f4209246163
leanhtai01 <leanhtai01@gmail.com>
2021-11-02 11:04:48 +0700

    post install: add script create apache connection string environment variable

---
+++ post_install/create_apache_conn_str_env_var.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+username=leanhtai01
+password=123
+hostname=localhost
+port=3306
+database=basicfb_db
+varname=JAWSDB_MARIA_URL
+
+mkdir -p /home/$(whoami)/apache_config/
+printf "SetEnv $varname \"mysql://$username:$password@$hostname:$port/$database\"\n" > /home/$(whoami)/apache_config/environment_variables.conf
