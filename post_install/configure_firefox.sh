8cc44412463a28a43acac8796a7c53aeeae235a4
leanhtai01 <leanhtai01@gmail.com>
2021-11-03 11:44:49 +0700

    post install: configure firefox: add newline

--- post_install/configure_firefox.sh
+++ post_install/configure_firefox.sh
@@ -3,4 +3,4 @@
 set -e
 
 mkdir -p /home/$(whoami)/.config/environment.d
-printf "MOZ_ENABLE_WAYLAND=1" > /home/$(whoami)/.config/environment.d/firefox.conf
+printf "MOZ_ENABLE_WAYLAND=1\n" > /home/$(whoami)/.config/environment.d/firefox.conf
