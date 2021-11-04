ad6af70cd79d8088620d3a6a6088e8abb933bfee
leanhtai01 <leanhtai01@gmail.com>
2021-11-04 13:38:45 +0700

    post install: configure editor: add config for root

--- post_install/configure_editor.sh
+++ post_install/configure_editor.sh
@@ -5,13 +5,19 @@ set -e
 username=$1
 current_dir=$(dirname $0)
 
-function getconfig() {
-    cd /home/${username}
-    mkdir -p backup_files_emacs
-    mkdir -p todo_list
-    git clone https://github.com/leanhtai01/.emacs.d
+function get_config_user() {
+    mkdir -p /home/${username}/backup_files_emacs
+    mkdir -p /home/${username}/todo_list
+    git clone https://github.com/leanhtai01/.emacs.d /home/${username}/.emacs.d
+}
+
+function get_config_root() {
+    sudo mkdir -p /root/backup_files_emacs
+    sudo mkdir -p /root/todo_list
+    sudo git clone https://github.com/leanhtai01/.emacs.d /root/.emacs.d
 }
 
 $current_dir/install_lsp_server.sh
-getconfig
+get_config_user
+get_config_root
 
