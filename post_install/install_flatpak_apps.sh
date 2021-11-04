716a19120f848127e0907df1cda4038bf7a410d2
leanhtai01 <leanhtai01@gmail.com>
2021-11-04 08:40:57 +0700

    post install: install flatpak apps: not install Flightgear

--- post_install/install_flatpak_apps.sh
+++ post_install/install_flatpak_apps.sh
@@ -8,5 +8,5 @@ flatpak install com.belmoussaoui.Authenticator -y
 # flatpak install com.getferdi.Ferdi -y
 flatpak install io.github.hakuneko.HakuNeko -y
 # flatpak install com.albiononline.AlbionOnline -y
-flatpak install org.flightgear.FlightGear -y
+# flatpak install org.flightgear.FlightGear -y
 flatpak install us.zoom.Zoom -y
