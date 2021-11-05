05c5d5f43b0d47845d4e04aabaf4c14917b09c62
leanhtai01 <leanhtai01@gmail.com>
2021-11-05 10:04:23 +0700

    post install: configure pipewire: using maximum resampling quality

--- post_install/configure_pipewire.sh
+++ post_install/configure_pipewire.sh
@@ -4,6 +4,6 @@ set -e
 
 mkdir -p ~/.config/pipewire
 cp -r /usr/share/pipewire/* ~/.config/pipewire
-sed -i '/resample.quality/s/#//; /resample.quality/s/4/10/' ~/.config/pipewire/{client.conf,pipewire-pulse.conf}
+sed -i '/resample.quality/s/#//; /resample.quality/s/4/15/' ~/.config/pipewire/{client.conf,pipewire-pulse.conf}
 sed -i '/suspend-node/s/suspend-node/#suspend-node/' ~/.config/pipewire/media-session.d/media-session.conf
 systemctl --user enable pipewire-media-session.service
