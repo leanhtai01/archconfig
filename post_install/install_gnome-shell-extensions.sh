c99c1b6c1534205beefaf18f13d951c1ba0d1cb8
leanhtai01 <leanhtai01@gmail.com>
2021-11-05 20:40:45 +0700

    post install: install gnome shell extensions: install switcher from github repos

--- post_install/install_gnome-shell-extensions.sh
+++ post_install/install_gnome-shell-extensions.sh
@@ -3,5 +3,7 @@
 set -e
 
 install_command="yay -Syu --needed --noconfirm"
+current_dir=$(dirname $0)
 
 $install_command gnome-shell-extension-appindicator-git gnome-shell-extension-vitals-git
+$current_dir/install_switcher_gnome_shell_ext.sh
