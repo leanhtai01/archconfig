5947704ea997c6e29b987544a1852d3ccf78dc96
leanhtai01 <leanhtai01@gmail.com>
2021-11-05 10:37:41 +0700

    post install: configure pipewire: install easyeffects from flatpak

--- post_install/configure_pipewire.sh
+++ post_install/configure_pipewire.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# install easyeffects from flatpak
+sudo pacman -Syu --noconfirm
+flatpak update
+flatpak install com.github.wwmm.easyeffects -y
+
 mkdir -p ~/.config/pipewire
 cp -r /usr/share/pipewire/* ~/.config/pipewire
 sed -i '/resample.quality/s/#//; /resample.quality/s/4/15/' ~/.config/pipewire/{client.conf,pipewire-pulse.conf}
