cf5be6d68a55b24c64f195d165c17db861f14034
leanhtai01 <leanhtai01@gmail.com>
2021-11-05 10:21:46 +0700

    system install: install audio: not install easyeffects from main repos

--- system_install/install_audio.sh
+++ system_install/install_audio.sh
@@ -14,7 +14,7 @@ install_command="${prefix}pacman -Syu --needed --noconfirm"
 re="[yY]"
 if [[ $install_pipewire_audio_server =~ $re ]]
 then
-    $install_command pipewire pipewire-pulse pipewire-alsa alsa-utils easyeffects xdg-desktop-portal-gtk gst-plugin-pipewire helvum lib32-pipewire pipewire-jack lib32-pipewire-jack
+    $install_command pipewire pipewire-pulse pipewire-alsa alsa-utils xdg-desktop-portal-gtk gst-plugin-pipewire helvum lib32-pipewire pipewire-jack lib32-pipewire-jack
 else
     $install_command pulseaudio pulseaudio-alsa alsa-utils pavucontrol pulseaudio-equalizer-ladspa
 fi
