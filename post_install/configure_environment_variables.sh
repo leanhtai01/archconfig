0c33772a8c554e390536b06c372cfc85a119dc76
leanhtai01 <leanhtai01@gmail.com>
2021-11-07 23:28:51 +0700

    post install: configure env vars: set EDITOR env var

--- post_install/configure_environment_variables.sh
+++ post_install/configure_environment_variables.sh
@@ -3,4 +3,5 @@
 set -e
 
 mkdir -p /home/$(whoami)/.config/environment.d
-printf "MOZ_ENABLE_WAYLAND=1\n" > /home/$(whoami)/.config/environment.d/firefox.conf
+# printf "MOZ_ENABLE_WAYLAND=1\n" > /home/$(whoami)/.config/environment.d/firefox.conf
+printf "EDITOR=emacs\n" > /home/$(whoami)/.config/environment.d/editor.conf
