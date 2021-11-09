5a4d506f297a9e5c43ced66ca783b9b1744f448a
leanhtai01 <leanhtai01@gmail.com>
2021-11-09 09:02:04 +0700

    post install: configure env var: add user's custom path

--- post_install/configure_environment_variables.sh
+++ post_install/configure_environment_variables.sh
@@ -5,3 +5,4 @@ set -e
 mkdir -p /home/$(whoami)/.config/environment.d
 # printf "MOZ_ENABLE_WAYLAND=1\n" > /home/$(whoami)/.config/environment.d/firefox.conf
 printf "EDITOR=emacs\n" > /home/$(whoami)/.config/environment.d/editor.conf
+printf "PATH=\"${PATH}:/home/$(whoami)/.config/composer/vendor/bin\"\n" > /home/$(whoami)/.config/environment.d/custom_path.conf
