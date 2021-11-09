f6b9371fd982710c2b7c125bedb110c2c887baed
leanhtai01 <leanhtai01@gmail.com>
2021-11-10 00:21:00 +0700

    system install: add script install_php_programming_env.sh

---
+++ system_install/install_php_programming_env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+prefix=
+
+if [ ! -z $1 ] && [ $1 = "in_chroot" ]
+then
+    prefix="arch-chroot /mnt "
+fi
+
+install_command="${prefix}pacman -Syu --needed --noconfirm"
+
+$install_command php composer php-tidy php-sqlite php-apache php-gd
