1163d660229390abfe4f21168c5f053dcbe633c1
leanhtai01 <leanhtai01@gmail.com>
2021-11-22 14:24:36 +0700

    tutorial: add example script ensure only one instance of script running

---
+++ tutorial/ensure_only_one_instance_of_script_running.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# must be using /bin/bash not /usr/bin/env bash for pidof (see pidof manpage)
+
+set -e
+
+if pidof -x $(basename $0) -o %PPID > /dev/null
+then
+    printf "Already running!\n";
+else
+    printf "Do something!\n"
+    sleep 20
+fi
