32546eba9bebf9403113ca60e99cac31832efa9f
leanhtai01 <leanhtai01@gmail.com>
2021-11-22 15:01:01 +0700

    tutorial: add example script rsync backup

---
+++ tutorial/rsync_backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+# rsync -a --delete \
+#       --exclude={"dir2","dir3","hello.txt"} \
+#       source/ dest/
+
+rsync -a --delete \
+      --exclude "dir2" \
+      --exclude "dir3" \
+      --exclude "hello.txt" \
+      source/ dest/
