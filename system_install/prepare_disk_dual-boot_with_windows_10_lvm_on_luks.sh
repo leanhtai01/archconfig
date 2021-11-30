1babeaf80c447c607de970987f34cf386b3fd64c
leanhtai01 <leanhtai01@gmail.com>
2021-11-30 12:52:11 +0700

    system install: prepare disk dual-boot Windows LVM on LUKS: systemd-boot: using XBOOTLDR

--- system_install/prepare_disk_dual-boot_with_windows_10_lvm_on_luks.sh
+++ system_install/prepare_disk_dual-boot_with_windows_10_lvm_on_luks.sh
@@ -4,11 +4,17 @@ set -e
 
 # variables
 efi_partnum=1
+xbootldr_partnum=5
 boot_partnum=5
 luks_partnum=6
 
 # partition the disk
 case $bootloader in
+    1) # systemd-boot
+        sgdisk -n 0:0:+1G -t 0:ea00 -c 0:"XBOOTLDR" /dev/$install_dev
+        wipefs -a /dev/${install_dev}${part}${xbootldr_partnum}
+        mkfs.vfat -F32 /dev/${install_dev}${part}${xbootldr_partnum}
+        ;;
     2) # GRUB (encrypted boot)
 	sgdisk -n 0:0:+1G -t 0:8309 -c 0:"cryptboot" /dev/$install_dev
 	;;
@@ -19,9 +25,7 @@ esac
 sgdisk -n 0:0:0 -t 0:8309 -c 0:"cryptlvm" /dev/$install_dev
 
 case $bootloader in
-    1) # systemd-boot
-	luks_partnum=5
-	
+    1) # systemd-boot	
 	# create the LUKS encrypted container
         wipefs -a /dev/${install_dev}${part}${luks_partnum}
 	printf "$storagepass1" | cryptsetup luksFormat --type luks2 /dev/${install_dev}${part}${luks_partnum} -
@@ -83,8 +87,10 @@ mkfs.ext4 /dev/sys_vol_group/root
 mount /dev/sys_vol_group/root /mnt
 case $bootloader in
     1) # systemd-boot
+        mkdir /mnt/efi
 	mkdir /mnt/boot
-	mount /dev/${install_dev}${part}${efi_partnum} /mnt/boot
+	mount /dev/${install_dev}${part}${efi_partnum} /mnt/efi
+        mount /dev/${install_dev}${part}${xbootldr_partnum} /mnt/boot
 	;;
     2) # GRUB (encrypted boot)
 	mkdir /mnt/boot
