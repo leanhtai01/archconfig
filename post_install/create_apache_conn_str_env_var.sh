29fe286ad97ab7c4d9c8ec4b84f86ce232cd773c
leanhtai01 <leanhtai01@gmail.com>
2021-12-04 09:03:41 +0700

    change apache env var name

--- post_install/create_apache_conn_str_env_var.sh
+++ post_install/create_apache_conn_str_env_var.sh
@@ -7,7 +7,7 @@ password=123
 hostname=localhost
 port=3306
 database=basicfb_db
-varname=JAWSDB_MARIA_URL
+varname=DB_URL
 
 mkdir -p /home/$(whoami)/apache_config/
 printf "SetEnv $varname \"mysql://$username:$password@$hostname:$port/$database\"\n" > /home/$(whoami)/apache_config/environment_variables.conf
