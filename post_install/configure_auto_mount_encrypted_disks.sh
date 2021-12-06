0ee07ea5a2dd66f1c83a7fbd7b4bbecde5425261
leanhtai01 <leanhtai01@gmail.com>
2021-12-06 16:50:47 +0700

    post install: mount encrypted disk: mount before httpd using systemd

--- post_install/configure_auto_mount_encrypted_disks.sh
+++ post_install/configure_auto_mount_encrypted_disks.sh
@@ -16,4 +16,4 @@ printf "$password" | sudo tee /etc/luks-keys/luks_data_drive > /dev/null
 sudo chmod 600 /etc/luks-keys/
 
 printf 'luks_data_drive UUID=4d9848e9-a41c-41b9-aadf-b514abd02ecc /etc/luks-keys/luks_data_drive nofail\n' | sudo tee -a /etc/crypttab > /dev/null
-printf '/dev/disk/by-uuid/5650d5fa-aec0-4941-889f-6b785f016ca6 /run/media/leanhtai01/data_drive auto nosuid,nodev,nofail,x-gvfs-show 0 0\n' | sudo tee -a /etc/fstab > /dev/null
+printf '/dev/disk/by-uuid/5650d5fa-aec0-4941-889f-6b785f016ca6 /run/media/leanhtai01/data_drive auto nosuid,nodev,nofail,x-gvfs-show,x-systemd.before=httpd.service 0 0\n' | sudo tee -a /etc/fstab > /dev/null
