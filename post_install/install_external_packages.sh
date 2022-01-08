2f4ceab8dc85f8955368a7bdc34b4172a67b6fd2
leanhtai01 <leanhtai01@gmail.com>
2022-01-08 21:33:40 +0700

    install vscode and sublime tools

--- post_install/install_external_packages.sh
+++ post_install/install_external_packages.sh
@@ -13,7 +13,7 @@ fi
 
 $current_dir/install_virtualbox-ext-oracle.sh
 $current_dir/install_ibus-bamboo.sh $2
-# $current_dir/install_visual_studio_code.sh
+$current_dir/install_visual_studio_code.sh
 $current_dir/install_flatpak_apps.sh
 $current_dir/install_google-chrome.sh
 # $current_dir/install_gnome-shell-extensions.sh
@@ -29,7 +29,7 @@ then
     $current_dir/install_ventoy-bin.sh
 fi
 
-# $current_dir/install_sublime_tools.sh
+$current_dir/install_sublime_tools.sh
 # $current_dir/install_dislocker.sh
 # $current_dir/install_mssql.sh
 # $current_dir/install_azure_data_studio.sh
