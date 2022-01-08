5faa3fdedc040905603a510179bb38ea370e04dd
leanhtai01 <leanhtai01@gmail.com>
2022-01-08 21:39:06 +0700

    install jetbrains toolbox and vdhcoapp

--- post_install/install_optional_external_packages.sh
+++ post_install/install_optional_external_packages.sh
@@ -5,10 +5,10 @@ set -e
 current_dir=$(dirname $0)
 
 $current_dir/install_birdtray.sh
-# $current_dir/install_vdhcoapp.sh
+$current_dir/install_vdhcoapp.sh
 # $current_dir/install_zoom.sh
 # $current_dir/install_skype.sh
-# $current_dir/install_jetbrains-toolbox.sh
+$current_dir/install_jetbrains-toolbox.sh
 # $current_dir/install_logisim-evolution.sh
 # $current_dir/install_qtspim.sh
 # $current_dir/install_bna.sh
