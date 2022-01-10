0c620ab9b1bdd016a4bdd4c5cc51a2865fc4f815
leanhtai01 <leanhtai01@gmail.com>
2022-01-10 14:10:17 +0700

    post install: configure editor: not create backup_files_emacs and todo_list directory

--- post_install/configure_editor.sh
+++ post_install/configure_editor.sh
@@ -6,14 +6,14 @@ username=$1
 current_dir=$(dirname $0)
 
 function get_config_user() {
-    mkdir -p /home/${username}/backup_files_emacs
-    mkdir -p /home/${username}/todo_list
+    # mkdir -p /home/${username}/backup_files_emacs
+    # mkdir -p /home/${username}/todo_list
     git clone https://github.com/leanhtai01/.emacs.d /home/${username}/.emacs.d
 }
 
 function get_config_root() {
-    sudo mkdir -p /root/backup_files_emacs
-    sudo mkdir -p /root/todo_list
+    # sudo mkdir -p /root/backup_files_emacs
+    # sudo mkdir -p /root/todo_list
     sudo git clone https://github.com/leanhtai01/.emacs.d /root/.emacs.d
 }
 
