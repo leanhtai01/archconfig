c0a1bd363bbc493f6debb3b082ed5755b49ce490
leanhtai01 <leanhtai01@gmail.com>
2022-01-14 23:13:35 +0700

    post install: install sublime tools: install only sublime-merge

--- post_install/install_sublime_tools.sh
+++ post_install/install_sublime_tools.sh
@@ -9,4 +9,4 @@ curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --
 echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
 
 # update pacman and install Sublime tools
-sudo pacman -Syu --needed --noconfirm sublime-text sublime-merge
+sudo pacman -Syu --needed --noconfirm sublime-merge # sublime-text
