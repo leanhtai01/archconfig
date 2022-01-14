debd11ced95dd555d967e4d03622697ed0d9f406
leanhtai01 <leanhtai01@gmail.com>
2022-01-14 13:38:16 +0700

    post install: configure editor: clone emacs config to XDG_CONFIG_HOME

--- post_install/configure_editor.sh
+++ post_install/configure_editor.sh
@@ -8,13 +8,13 @@ current_dir=$(dirname $0)
 function get_config_user() {
     # mkdir -p /home/${username}/backup_files_emacs
     # mkdir -p /home/${username}/todo_list
-    git clone https://github.com/leanhtai01/.emacs.d /home/${username}/.emacs.d
+    git clone https://github.com/leanhtai01/emacsconfig /home/${username}/.config/emacs
 }
 
 function get_config_root() {
     # sudo mkdir -p /root/backup_files_emacs
     # sudo mkdir -p /root/todo_list
-    sudo git clone https://github.com/leanhtai01/.emacs.d /root/.emacs.d
+    sudo git clone https://github.com/leanhtai01/emacsconfig /root/.config/emacs
 }
 
 $current_dir/install_lsp_server.sh
