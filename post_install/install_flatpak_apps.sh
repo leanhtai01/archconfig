a2484e80bd9383492e4b59c68000af59f7aa4fd5
leanhtai01 <leanhtai01@gmail.com>
2022-01-14 23:10:48 +0700

    post install: install flatpak apps: not install Hakuneko

--- post_install/install_flatpak_apps.sh
+++ post_install/install_flatpak_apps.sh
@@ -6,7 +6,7 @@ sudo pacman -Syu --noconfirm
 flatpak update
 flatpak install com.belmoussaoui.Authenticator -y
 # flatpak install com.getferdi.Ferdi -y
-flatpak install io.github.hakuneko.HakuNeko -y
+# flatpak install io.github.hakuneko.HakuNeko -y
 # flatpak install com.albiononline.AlbionOnline -y
 # flatpak install org.flightgear.FlightGear -y
 # flatpak install us.zoom.Zoom -y
