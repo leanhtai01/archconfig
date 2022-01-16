22851be8729c2e0d664192dd30aa7bce6870d4d7
leanhtai01 <leanhtai01@gmail.com>
2022-01-16 14:37:10 +0700

    post install: install optional external package: not install birdtray and vdhcoapp

--- post_install/install_optional_external_packages.sh
+++ post_install/install_optional_external_packages.sh
@@ -4,8 +4,8 @@ set -e
 
 current_dir=$(dirname $0)
 
-$current_dir/install_birdtray.sh
-$current_dir/install_vdhcoapp.sh
+# $current_dir/install_birdtray.sh
+# $current_dir/install_vdhcoapp.sh
 # $current_dir/install_zoom.sh
 # $current_dir/install_skype.sh
 $current_dir/install_jetbrains-toolbox.sh
