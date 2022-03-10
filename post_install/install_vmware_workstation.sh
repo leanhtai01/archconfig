e9af711a0d57f6d18572d3ef8bef23c91a7b07d8
leanhtai01 <leanhtai01@gmail.com>
2022-03-10 08:05:50 +0700

    post install: fix error when install vmware workstation

--- post_install/install_vmware_workstation.sh
+++ post_install/install_vmware_workstation.sh
@@ -5,5 +5,5 @@ set -e
 yay -Syu --noconfirm vmware-workstation
 sudo systemctl enable vmware-networks
 sudo systemctl enable vmware-usbarbitrator
-sudo modprobe -a vmw_vmci vmmon
+# sudo modprobe -a vmw_vmci vmmon
 sudo /usr/lib/vmware/bin/vmware-vmx-debug --new-sn ZF3R0-FHED2-M80TY-8QYGC-NPKYF
