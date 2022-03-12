0693dccaa8e3dad027cbeedb90f0b5559d1b6efc
leanhtai01 <leanhtai01@gmail.com>
2022-03-12 09:45:46 +0700

    system install: install logographic fonts

--- system_install/install_core_packages.sh
+++ system_install/install_core_packages.sh
@@ -13,7 +13,7 @@ install_command="${prefix}pacman -Syu --needed --noconfirm"
 
 # install and configure some packages, services
 # fonts
-$install_command ttf-dejavu ttf-liberation noto-fonts-emoji ttf-cascadia-code # adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-serif-cn-fonts adobe-source-han-serif-tw-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts ttf-fira-code ttf-roboto-mono ttf-hack
+$install_command ttf-dejavu ttf-liberation noto-fonts-emoji ttf-cascadia-code adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-serif-cn-fonts adobe-source-han-serif-tw-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts ttf-fira-code ttf-roboto-mono ttf-hack
 
 # browsers
 $install_command torbrowser-launcher firefox-developer-edition chromium
