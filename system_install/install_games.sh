a820f99900cc2154fa98c6ebca748886b03b6428
leanhtai01 <leanhtai01@gmail.com>
2022-03-12 08:46:36 +0700

    system install: install games: install lutris, wine

--- system_install/install_games.sh
+++ system_install/install_games.sh
@@ -11,13 +11,13 @@ fi
 
 install_command="${prefix}pacman -Syu --needed --noconfirm"
 
-# # install lutris and its all optional dependencies
-# $install_command lutris
-# $install_command $(printf "$(${prefix}pacman -Qi lutris)" | sed -n '/^Optional Deps/,$p' | sed '/^Required By/q' | head -n -1 | cut -c19- | cut -d[ -f1 | cut -d: -f1)
+# install lutris and its all optional dependencies
+$install_command lutris
+$install_command $(printf "$(${prefix}pacman -Qi lutris)" | sed -n '/^Optional Deps/,$p' | sed '/^Required By/q' | head -n -1 | cut -c19- | cut -d[ -f1 | cut -d: -f1)
 
-# # install wine and its all dependencies
-# $install_command wine
-# $install_command $(printf "$(${prefix}pacman -Qi wine)" | sed -n '/^Optional Deps/,$p' | sed '/^Required By/q' | head -n -1 | cut -c19- | cut -d[ -f1 | cut -d: -f1)
+# install wine and its all dependencies
+$install_command wine
+$install_command $(printf "$(${prefix}pacman -Qi wine)" | sed -n '/^Optional Deps/,$p' | sed '/^Required By/q' | head -n -1 | cut -c19- | cut -d[ -f1 | cut -d: -f1)
 
 # install steam
 $install_command steam steam-native-runtime
