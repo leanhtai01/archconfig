55813803c9719731015c6c3f26c3cd7eff96215a
leanhtai01 <leanhtai01@gmail.com>
2022-04-21 14:59:41 +0700

    post install: install dislocker

--- post_install/install_external_packages.sh
+++ post_install/install_external_packages.sh
@@ -32,7 +32,7 @@ fi
 $current_dir/install_goldendict.sh
 $current_dir/install_sublime-merge.sh
 # $current_dir/install_sublime_tools.sh
-# $current_dir/install_dislocker.sh
+$current_dir/install_dislocker.sh
 # $current_dir/install_mssql.sh
 # $current_dir/install_azure_data_studio.sh
 # $current_dir/install_heroku.sh
